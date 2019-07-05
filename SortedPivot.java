f84a6b62fe0b6414d2953a3eb6101e5797c2bc06
Arivoli AE <aehari2010@gmail.com>
2019-07-04 17:17:00 -0700

    Commit for Day 3 - Changes to Problem #2

--- SortedPivot.java
+++ SortedPivot.java
@@ -1,24 +1,43 @@
 class SortedPivot {
 
     public int findPivot(int[] nums, int low, int high){
-               
-        for(int i=low;i<high;i++){
-            if(nums[i] > nums[i+1]){
-                return i+1;
-            }
+        
+        //null check
+        if(nums.length == 0)
+            return -1;
+
+        //isPivot zero check
+        if(nums[0] <= nums[nums.length-1])
+            return 0;
+        
+        //binary pivot
+        if(low > high){
+            return -1;
+        }        
+        
+        int mid = low + (high-low)/2;
+        if(nums[mid] > nums[mid+1])
+            return mid+1;
+        
+        if(nums[low] <= nums[mid]){
+            return findPivot(nums, mid+1, high);
         }
-        return 0;
+        else{
+            return findPivot(nums, low, mid-1);
+        }
     }
 
     public int binarySearch(int[] nums, int low, int high, int target){
-        if(low > high)
+        if(nums.length == 0)
             return -1;
-        
-        int mid = (low+high)/2;
+        if(low > high)
+            return -1;        
+        int mid = low + (high-low)/2;
         if(nums[mid] == target)
             return mid;
         
         if(nums[mid] < target){
+            System.out.println(mid+1);
             return binarySearch(nums, mid+1, high, target);
         }
         else{
@@ -32,20 +51,21 @@ class SortedPivot {
      int low = 0; int high = nums.length-1;
      int pivot = findPivot(nums, low, high);
     
-     //binary search
-     int left = binarySearch(nums, low, pivot-1, target);
-     int right = binarySearch(nums, pivot, high, target);
-
-     if(left == -1){
-         return right;
-     }
-     else{
-         return left;
-     }    
+     //binary search     
+        int left = binarySearch(nums, low, pivot-1, target);
+        int right = binarySearch(nums, pivot, high, target);
+   
+        if(left == -1){
+            return right;
+        }
+        else{
+            return left;
+        }             
+         
     }
     public static void main(String[] args){
         System.out.println("Inside Sorted Pivot Implementation");
-        int[] arr = {4,5,6,7,0,1,2};
+        int[] arr = {1};
         int target = 0;
         SortedPivot obj = new SortedPivot();
 

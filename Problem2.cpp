79581eba36897a6790004a6be5e2a5c7e8724450
shazmaan <shazmaan@buffalo.edu>
2019-07-05 05:44:18 +0400

    Made tiny changes to work better.

--- Problem2.cpp
+++ Problem2.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 class Problem2{
+public:
     int prob(vector<int>& vec, int target){
         int low = 0; int high = 1; int val = vec[low];
         while(val<target){
@@ -19,24 +20,35 @@ class Problem2{
                 return MID(vec,low,high,target);
             }
         }
+        return MID(vec,low,high,target);
     }
-    int MID(vector<int>& vec,int l, int h, int value){
-        int low = l; int high = h; int mid;
-        while(low<high){
-            mid = (low +high)/2;
-            try{
+    int MID(vector<int>& vec,int l, int h, int value) {
+        int low = l;
+        int high = h;
+        int mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            try {
                 int a = vec[mid];
-                if(a==value){
+                if (a == value) {
                     return mid;
-                }else if(a<value){
-                    low = mid;
-                }else{
-                    high = mid;
+                } else if (a < value) {
+                    low = mid + 1;
+                } else{
+                    high = mid - 1;
                 }
-            }catch(const std::out_of_range& e){
+
+            } catch (const std::out_of_range &e) {
                 continue;
             }
         }
         return -1;
     }
 };
+
+int main(){
+    Problem2 p;
+    vector<int>  vec{1,2,3,4,5,6};
+    cout<<p.prob(vec,6)<<endl;
+    return 0;
+}

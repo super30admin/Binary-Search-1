db03bfc8887fa595463a35e98d01aef8ff18bd56
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-11-20 00:45:25 -0800

    Updated changes

--- Searching_rotated_sorted_array.py
+++ Searching_rotated_sorted_array.py
@@ -1,7 +1,13 @@
+# running succesfully on leet code
+# missed two edge cases and took time to arrive to the solution
+
+# memory complexity: I think it is O(1), could you confirm?
+# runtime complexity: O(logn) ----eliminates half elements in each recusion step performed
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # a recursive function which knows limits in which it should operate
-        def binary(nums, target, l, h):
+        def helper(nums, target, l, h):
             #while valid indexes perform recursion
             if l <= h:
                 # calculating mid point
@@ -15,21 +21,21 @@ class Solution:
                         return l
                     if target < nums[m] and target > nums[l]: 
                         # search left side
-                        return binary(nums, target, l, m - 1)
+                        return helper(nums, target, l, m - 1)
                     else:
                         # search right side
-                        return binary(nums, target, m+1, h) 
+                        return helper(nums, target, m+1, h) 
                 else:
                     if target == nums[h]:
                         #return index
                         return h
                     if target < nums[h] and target > nums[m]:
                          # search right side
-                        return binary(nums, target, m+1, h)
+                        return helper(nums, target, m+1, h)
                     else:
                         # search left side
-                        return binary(nums, target, l, m - 1) 
+                        return helper(nums, target, l, m - 1) 
             #when invalid indexes implicates that number is not present. return -1
             else:
                 return -1
-        return binary(nums, target, 0, len(nums) - 1)
+        return helper(nums, target, 0, len(nums) - 1)

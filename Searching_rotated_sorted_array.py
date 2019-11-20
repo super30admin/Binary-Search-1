4a5314349e0208c5e3fb3fea5b5497ac149f2015
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-11-20 00:41:11 -0800

    Added 1 question

---
+++ Searching_rotated_sorted_array.py
@@ -0,0 +1,35 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # a recursive function which knows limits in which it should operate
+        def binary(nums, target, l, h):
+            #while valid indexes perform recursion
+            if l <= h:
+                # calculating mid point
+                m = l + (h-l) // 2
+                if nums[m] == target:
+                    return m
+                # left side is sorted
+                if nums[m] > nums[l]:  
+                    if target == nums[l]:
+                        # return index as it is equal to target
+                        return l
+                    if target < nums[m] and target > nums[l]: 
+                        # search left side
+                        return binary(nums, target, l, m - 1)
+                    else:
+                        # search right side
+                        return binary(nums, target, m+1, h) 
+                else:
+                    if target == nums[h]:
+                        #return index
+                        return h
+                    if target < nums[h] and target > nums[m]:
+                         # search right side
+                        return binary(nums, target, m+1, h)
+                    else:
+                        # search left side
+                        return binary(nums, target, l, m - 1) 
+            #when invalid indexes implicates that number is not present. return -1
+            else:
+                return -1
+        return binary(nums, target, 0, len(nums) - 1)

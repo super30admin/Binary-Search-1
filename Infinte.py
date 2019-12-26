0b64f53c9276f957ab51164ebf24840aa0182161
Neha Sharma <53287893+neha-0507@users.noreply.github.com>
2019-12-26 12:55:09 -0700

    Create Infinte.py

---
+++ Infinte.py
@@ -0,0 +1,26 @@
+// Time Complexity :O(logn)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+class solution(object):
+    def search(self,nums,l,r,target):
+        l=0
+        r=len(nums)-1
+        while(l<=r):
+            mid=(l+r)//2
+            if(target==nums[mid]):
+                return target
+            elif(nums[mid]>target):
+                r=mid-1
+            else:
+                l=mid+1
+    def findpos(arr,key):
+        l=0
+        h=1
+        val=nums[0]
+        while(val<key):
+            l=h
+            r=2*h
+            val=nums[h]
+            
+        return search(arr,l,h,key)

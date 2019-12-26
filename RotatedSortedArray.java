daac3949fae3b64f4e47ed90d3f5833dbca4fe27
Manasa Hari <harimanasa17@gmail.com>
2019-12-25 22:46:03 -0800

    Update RotatedSortedArray.java

--- RotatedSortedArray.java
+++ RotatedSortedArray.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(logN)
+// Space Complexity : O(1) in iterative method and O(logN) in recursive method
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
 class Solution {
     public int search(int[] nums, int target) {
         int result;

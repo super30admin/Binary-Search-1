0ff2d4e4d8e1a0ff456dcb6a32905fb3a7ef8d37
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-05 08:20:37 -0500

    all problems complete updated as per class

--- infinite_sorted_array.py
+++ infinite_sorted_array.py
@@ -9,34 +9,34 @@
 
 class Solution:
 
-    def infinite_sorted_array(self, nums: list, target: int) -> int:
-        """
-        UPDATED AS PER CLASS DISCUSSION
-        // Time Complexity :
-                            O(logn) 'n' is the target value
-        // Space Complexity :
-                            O(1)
-        // Did this code successfully run on Leetcode :
-                            N/A
-        // Any problem you faced while coding this :
-                            No
-        // Your code here along with comments explaining the approach
-            We don't need to check for nums[start] > target because the start
-            is adjusted each time. The element to be searched for is for sure
-            greater than start because if it was lesser or equal the while loop
-            would have exited
-        """
-        start = 0
-        end = 2
-        # break when end is greater or equal to target
-        # else search for the range
-        # if we have nums[end] <= target we don't have any
-        # problem its just the number of elements search for is more
-        while nums[end] < target:
-            # until we reach end number is lesser we keep increasing the range
-            start = end
-            end *= 2
-        return self.binarySearch(nums, start, end, target)
+    # def infinite_sorted_array(self, nums: list, target: int) -> int:
+    #     """
+    #     UPDATED AS PER CLASS DISCUSSION
+    #     // Time Complexity :
+    #                         O(logn) 'n' is the target value
+    #     // Space Complexity :
+    #                         O(1)
+    #     // Did this code successfully run on Leetcode :
+    #                         N/A
+    #     // Any problem you faced while coding this :
+    #                         No
+    #     // Your code here along with comments explaining the approach
+    #         We don't need to check for nums[start] > target because the start
+    #         is adjusted each time. The element to be searched for is for sure
+    #         greater than start because if it was lesser or equal the while loop
+    #         would have exited
+    #     """
+    #     start = 0
+    #     end = 2
+    #     # break when end is greater or equal to target
+    #     # else search for the range
+    #     # if we have nums[end] <= target we don't have any
+    #     # problem its just the number of elements search for is more
+    #     while nums[end] < target:
+    #         # until we reach end number is lesser we keep increasing the range
+    #         start = end
+    #         end *= 2
+    #     return self.binarySearch(nums, start, end, target)
 
     # def infinite_sorted_array(self, nums: list, target: int) -> int:
     #     """
@@ -71,15 +71,38 @@ class Solution:
     #         except IndexError:
     #             return -1
 
-    def binarySearch(self, nums: list, start: int, end: int, target: int) -> int:
-        while start <= end:
-            mid = (start + end) // 2
+    # def binarySearch(self, nums: list, start: int, end: int, target: int) -> int:
+    #     while start <= end:
+    #         mid = (start + end) // 2
+    #         if nums[mid] == target:
+    #             return mid
+    #         elif target < nums[mid]:
+    #             end = mid - 1
+    #         elif target > nums[mid]:
+    #             start = mid + 1
+    #     return -1
+    def infinite_sorted_array(self, nums: list, target: int) -> int:
+        low = 0
+        high = 2
+        while True:
+            if nums[high] == target:
+                return high
+            elif nums[high] > target:
+                break
+            else:
+                low = high
+                high *= 2
+        return self._binary_search(nums, low, high, target)
+
+    def _binary_search(self, nums: list, low: int, high: int, target: int) -> int:
+        while low <= high:
+            mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
-            elif target < nums[mid]:
-                end = mid - 1
-            elif target > nums[mid]:
-                start = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
         return -1
 
 
@@ -87,8 +110,8 @@ if __name__ == '__main__':
     h = Solution()
     print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 4))
     print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 2))
-    # print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 7))
-    # print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 100))
+    print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12], 7))
+    print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], -100))
     # print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 6))
     # print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], 1))
     # print(h.infinite_sorted_array([0, 1, 2, 4, 5, 6, 7], -11))

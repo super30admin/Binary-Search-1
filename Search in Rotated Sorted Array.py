3199efe4a0aecf6a310d41bd856646ed50606545
Lekha <47202180+lekhav@users.noreply.github.com>
2020-01-14 08:37:10 -0800

    Update Search in Rotated Sorted Array.py

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -28,7 +28,11 @@ class Solution:
                     
         return -1
             
-            
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+obj = Solution()
+print(obj.search(nums, target))            
         
         
             

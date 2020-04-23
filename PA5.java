63700c23ee123283efc7cc20a3782770637a779b
HimanshuAgarwal93 <himanshu.agarwal93@yahoo.com>
2020-04-23 16:47:35 -0500

    PA5 updated solution

--- PA5.java
+++ PA5.java
@@ -1,60 +1,37 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int [] res= {-1,-1};      
-        int low= 0;
-        int high = nums.length-1;
-        int mid= 0;
-        int index=-1;
-        while(low<=high){
-            mid= low+(high-low)/2;
-            if(nums[mid]==target){
-                if(mid<nums.length-1 && nums[mid+1]== target){
-                    low= mid+1;                   
-                }
-                else{
-                   
-                    res[1]= mid;
-                    break;
-                }
-            }
-            else{
-                  if(nums[mid]>target){
-                high= mid-1;
-            }
-            else{
-                low= mid+1;
-            }
-            }       
+    public int search(int[] nums, int target) {
+        if(nums.length ==0){
+            return -1;
         }
-        low= 0;
-        high= res[1];
-        mid=0;
-         while(low<=high){
-            mid= low+(high-low)/2;
-            if(nums[mid]==target){
-
-                if(mid>0 && nums[mid-1]== target){
- 
-                    high= mid-1;
-                    
-                }
-                else{
-   
-                    res[0]= mid;
-                    break;
-                }
+        
+        int i=0;
+        int j= nums.length-1;
+        
+        int mid=0;
+        while(i<=j){
+            mid= (i+j)/2;
+            
+            if(nums[mid]== target){
+                return mid;
             }
-             else{
-                 if(nums[mid]>target){
-                high= mid-1;
-            }
+           if(nums[mid]>=nums[i]){
+               if(target>=nums[i] && target<nums[mid]){
+                   j= mid-1;
+               }
+               else{
+                   i=mid+1;
+               }
+           }
             else{
-                low= mid+1;
+                if(target>nums[mid] && target<=nums[j]){
+                   i= mid+1;
+               }
+               else{
+                   j=mid-1;
+               }
             }
-             }
-            
-            
+        
         }
-        return res;    
+        return -1;
     }
 }

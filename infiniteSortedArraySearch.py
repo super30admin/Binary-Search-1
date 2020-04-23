b2e88a725042a907ce05f2e1f4b72db6dcd2cbc9
Sonali B Sreedhar <50754966+sbsreedh@users.noreply.github.com>
2020-04-22 19:49:11 -0700

    Updated Space complexity
    O(1)  is the SC as that is only the space taken up by the variable we create.

--- infiniteSortedArraySearch.py
+++ infiniteSortedArraySearch.py
@@ -2,7 +2,7 @@
 
 #Given a sorted array of unknown length and a number to search for, return the index of the number in the array. Accessing an element out of bounds throws exception. If the number occurs multiple times, return the index of any occurrence. If it isn’t present, return -1.
 #Time Complexity :O(log n)
-#Space Complexity :O(n)
+#Space Complexity :O(1)
 
 
 

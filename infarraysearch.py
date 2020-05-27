3055e8a706135ec69c4d5286b4a0423db7571b11
Abhishek Prusty <Abhishek.lan@gmail.com>
2020-05-27 01:20:04 -0500

    Added basecode for 2nd problem

---
+++ infarraysearch.py
@@ -0,0 +1,21 @@
+def interval_det(arr, lower_bound, upper_bound, target):
+	if target<upper_bound and target>lower_bound:
+		return binary_search(arr, lower_bound, upper_bound, target)
+	else:
+		return interval_det(arr, lower_bound+200, upper_bound+200, target)
+
+def binary_search(arr, low, high, target):
+	while low<=high:
+		mid = (low+high)//2
+		if arr[mid]==target:
+			return mid
+		elif arr[mid]>target:
+			high = mid-1
+		elif arr[mid]<target:
+			low = mid+1
+	return -1
+
+
+array = [......]#infinite array
+target = x
+print(interval_det(array, 0, 200, target))

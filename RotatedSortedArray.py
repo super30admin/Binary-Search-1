743f45ce545d5c88838bff713978e313edc61bad
Samarth Juneja <samarthj@buffalo.edu>
2020-05-27 23:11:10 +0530

    Exercise 1 RotatedSortedArray.py Completed with comments and time & space complexity

--- RotatedSortedArray.py
+++ RotatedSortedArray.py
@@ -1,32 +1,43 @@
-# Time Complexity :
-# Space Complexity :
-# Did this code successfully run on Leetcode :
-# Any problem you faced while coding this :
+# Time Complexity : O(log(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes!
+# Any problem you faced while coding this : I had an almost working recursive solution
+# which was working for every array but 1 but the concept was made clear after the session.
 
 # Your code here along with comments explaining your approach
 class Solution(object):
     def search(self, nums, target):
-        return self.binarySearch(nums, 0, len(nums) - 1, target)
-
-
-    def binarySearch(self, arr, left, right, x):
-        if left <= right:
+        if len(nums) == 0:
+            return -1
+        # bound variables for array traversal
+        left = 0
+        right = len(nums)-1
+        # terminating condition for while loop
+        while left <= right:
             mid = left + (right - left) / 2
-            if x == arr[mid]:
+            # if element present at mid index
+            if target == nums[mid]:
                 return mid
-            elif x < arr[mid]:
-                if arr[left] <= x < arr[mid]:
-                    return self.binarySearch(arr, left, mid-1, x)
+            # left array sorted
+            elif nums[left] <= nums[mid]:
+                # target present in left sorted array
+                if nums[left] <= target < nums[mid]:
+                    right = mid -1
+                # target present in right array
                 else:
-                    return self.binarySearch(arr, mid+1, right, x)
-            elif x > arr[mid]:
-                if arr[right] >= x > arr[mid]:
-                    return self.binarySearch(arr, mid+1, right, x)
+                    left = mid + 1
+            # else right array sorted
+            else:
+                # target present in right sorted array
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                # target present in left array
                 else:
-                    return self.binarySearch(arr, left, mid-1, x)
+                    right = mid - 1
+        # element not present
         return -1
 
-nums = [4, 5, 6, 7, 0, 1, 2]
-target = 4
+nums = [4,5,6,7,8,1,2,3]
+target = 8
 s = Solution()
-print s.search(nums, target)
+print(s.search(nums, target))

4ede4f376770cd2cd53a29d785f1bc6567eb914d
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-05-27 18:50:12 -0400

    Completed Sprint Binary-Search-1

---
+++ LeetCode702.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(log n) --> where n is size of input
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Didn't aware of this approach
+
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+	public int search(ArrayReader reader, int target) {
+		int low = 0; 
+		int high = 1;
+		
+		while (reader.get(high) < target) {
+			low = high;
+			high = high * 2;
+		}
+		
+		return binarySearch(reader, target, low, high);
+	}
+	
+	private int binarySearch(ArrayReader reader, int target, int low, int high) {
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			
+			if (reader.get(mid) == target) return mid;
+			else if (reader.get(mid) > target) high = mid - 1;
+			else low = mid + 1 
+		}
+		return -1;
+	}
+}
---
+++ RoatedArray1.java
@@ -0,0 +1,50 @@
+// Time Complexity : O(log n) --> where n is size of input
+// Space Complexity : O(log n) --> recursion stack space
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Didn't aware of this approach
+
+
+// Your code here along with comments explaining your approach
+// Using Recursion
+class Solution {
+    
+    public int search(int[] nums, int target) {
+        
+        if (nums == null || nums.length == 0) return -1;
+        
+        int low = 0;
+        int high = nums.length - 1;
+        
+        return binarySearch(nums, target, low, high);
+    }  
+    
+    private int binarySearch(int[] nums, int target, int low, int high) {
+        
+        // base case
+        if (low > high) return -1;
+        
+        // logic
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) return mid; 
+
+        else if (nums[low] <= nums[mid]) { // left side sorted
+            if (nums[low] <= target && target <= nums[mid]) {
+                high = mid -1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        else { // right side sorted
+            if (nums[mid] <= target && target <= nums[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    return binarySearch(nums, target, low, high);
+    }
+}

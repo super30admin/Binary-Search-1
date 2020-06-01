770dda86387f112e4d6d116315c5a17e0ea80add
saumilnpatel90 <56368073+saumilnpatel90@users.noreply.github.com>
2020-06-01 06:33:59 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,3 +1,9 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
 public class Solution {
 	
 	public int search(int[] nums, int target) {

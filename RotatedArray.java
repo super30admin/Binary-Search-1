77f84fc2e9ccc72c0a2f127e33ab2528808f6d3a
Ashwini Mangesh Jaitapkar <ashwini@Ashwinis-MacBook-Pro.local>
2020-07-08 17:13:32 -0400

    Search in Rotated Array Problem Solved

--- RotatedArray.java
+++ RotatedArray.java
@@ -1,45 +1,43 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+//1. define low , high and mid
+//2. Find which side is sorted
+//3. Adjust low and high accordingly and search in that particular direction
+
 class Solution {
     public int search(int[] nums, int target) {
+        int low =0;
+        int high = nums.length-1;
         
-        //1.Define low and high
-        //2.find mid//barrier
-        //3. find which side is sorted
-        //4.do binary search on that side 
-        int low=0;
-        int high= nums.length -1;   
         while(low<=high){
-            int mid =(low+high)/2;
-            
+            int mid = (low+high)/2;
             if(nums[mid]==target){
                 return mid;
-            }
-            
-            else if(nums[low]<=nums[mid]){
+        }else if(nums[low]<=nums[mid]){
                 //left side is sorted
+                
                 if(target>=nums[low]&&target<=nums[mid]){
-                    //it is in the middle 2 <=x<=4
-                    high = mid -1;
-                    
+                    //target is in middle 
+                    high=mid-1;
                 }else{
-                    low =mid+1;
+                    low = mid+1;
                 }
             }else{
-                
-                //left side is sorted
+                //right side is sorted
                 if(target>=nums[mid]&&target<=nums[high]){
-                    //it is in the middle 2 <=x<=4
-                   low = mid+1;
-                    
+                    low=mid+1;
                 }else{
                     high = mid-1;
                 }
+            }
+            
             
         }
+        return -1;
         
         
     }
-        return -1;
 }
-}
-
-//Time Complexity - O( log n)

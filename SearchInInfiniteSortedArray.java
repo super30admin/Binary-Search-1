14665a7ed035e3876e8c7e4ca2b6a84036461563
shashidhar-01 <66799707+shashidhar-01@users.noreply.github.com>
2020-07-10 19:50:58 -0500

    Binary Search 1 || Please review in Java

---
+++ SearchInInfiniteSortedArray.java
@@ -0,0 +1,29 @@
+// time complexity : O(logn)
+//space complexity : O(1)
+//executed on leetcode: yes
+//any issues faced: no
+
+class Solution{
+    public int search(ArrayReader reader,int target)
+    {
+        int low=0;
+        int high = 1;
+        while(reader.get(high)<target)
+        {
+            low = high;
+            high = 2 * high;
+        }
+        return binarySearch(reader,target,low,high);
+    }
+    private int binarySearch(ArrayReader reader, int target,int low,int high)
+    {
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(reader.get(mid)==target) return mid;
+            else if(reader.get(mid)>target) high = mid -1;
+            else low = mid + 1;
+        }
+        return -1;
+    }
+}

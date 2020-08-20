22600f0c8e02f7b04648c943a43ad848748cb10f
dcarao <ancarao@yahoo.co>
2020-08-20 00:04:59 -0500

    Binary search 1 update

--- SearchInInfinite.java
+++ SearchInInfinite.java
@@ -6,29 +6,27 @@
 
 // Your code here along with comments explaining your approach
 class Solution {
-    
-    public int search(int[] nums, int target) {
-        int left =0;
-        int right = nums.length -1;
+    public int search(ArrayReader reader, int target) {
+        int left = 0;
+        int right = 1;
         
-        while(left<=right){
+        while(reader.get(right) != Integer.MAX_VALUE){
+            right = right * 2;
+        }
+        return binarySearch(reader, left, right, target);
+    }
+    
+    private int binarySearch(ArrayReader reader, int left, int right, int target){
+        while(left <= right){
             int mid = left + (right - left)/2;
-            if(nums[mid]== target) return mid;
-            if(nums[mid] >= nums[left]){
-                if(target >=nums[left] && target< nums[mid]){
-                    right = mid - 1;
-                }else{
-                    left = mid + 1;
-                }
+            if(reader.get(mid) == target)
+                return mid;
+            else if(reader.get(mid) > target){
+                right = mid - 1;                
             }else{
-                if(target <=nums[right] && target > nums[mid]){
-                    left = mid + 1;
-                }else{
-                    right = mid -1;
-                }
+                left = mid +1;
             }
         }
         return -1;
-        
     }
 }

b1c5fc3d2aca0485e3abf6ca24b776dc9d045ffc
Pravin Jha <pravinjha@siu.com>
2020-08-20 19:22:47 -0500

    Revised problem

--- UnknownLengthArrayProblem.java
+++ UnknownLengthArrayProblem.java
@@ -1,24 +1,33 @@
 class Solution {
     public int search(ArrayReader reader, int target) {
-        int left=0, right=Integer.MAX_VALUE;
+        int left=0, right=1;
+         while(reader.get(right)!=Integer.MAX_VALUE)
+         {
+         	right=right*2;
+         }
+        return binarySearch(reader,target,left,right);
+    }
+    int binarySearch(ArrayReader reader,int target,int left,int right)
+    {
         while (left<=right) {
-            int mid=(left+right)/2; 
-            int x = reader.get(mid);
-            if (x==Integer.MAX_VALUE || x>target)
+            int mid=left+(right-left)/2; 
+            if(reader.get(mid)==target)
+            {
+                return mid;
+            }
+            else if(reader.get(mid)>target)
             {
                 right=mid-1; 
             }
-            else if (x<target) 
+            else  
             {
                 left=mid+1; 
             }
-            else 
-            {
-                return mid; 
-            }
+            
         }
         return -1; 
     }
         
-    
-}
+} 
+//Time complexity=O(logn)
+//Space complexity=O(1)

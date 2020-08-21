2a98648a8ec4ca4480347c7f663ce81e2fd7f0d0
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-08-20 17:12:37 -0700

    added search in infirnite array changes

--- searchinInfiniteSortedArray.java
+++ searchinInfiniteSortedArray.java
@@ -1,45 +1,71 @@
 
     /*  Explanation
-    Time Complexity for operators : log(n)   
+    Time Complexity for operators : log(n) + log(n) = log(n) 
     Extra Space Complexity for operators : o(1) .. No extra space
     Did this code successfully run on Leetcode : Yes
-    Any problem you faced while coding this : I forgot with one condition where in while loop  while(low<high). I forgot to check
-    low <= high. It failed initially.
-    The implemntation consists dividing array and checking the first element with the mid. It will tell which side
-    if already in sorted array. Once we found that, we need to travese on that side and check for target.
-    If it did not found then try on other side.
+    Any problem you faced while coding this : No
+    The implemntation consists dividing array and find the element. Once element is found traverse till left most
+    of the element and same for right side.
     */
 
-public class searchinRotatedSOrtedArray{
+public class searchinInfiniteSortedArray{
 
 
     public static void main(String args[]){
-            int arr[] = new int[]{4,5,6,7,0,1,2};
-            System.out.println(search(arr,0));
+            int arr[] = new int[]{5,7,7,8,8,10};
+            int arr1[] = searchRange(arr,8);
+            System.out.println(arr1[0]);
+            System.out.println(arr1[1]);
     }
-    public static int infiniteSortedArray(int[] nums, int target) {
-     
-        int low =0;
+
+    public static int[] searchRange(int[] nums, int target) {
+        int left = traverseLeft(nums,target);
+        int right = traverseRight(nums,target);
+           
+        return new int[]{left,right};
+    }
+   
+    public static int traverseLeft(int[] nums,int target){
+        int low = 0;
         int high = nums.length-1;
-        
-        while(low<=high){
+       
+        while(low <= high){
             int mid = low + (high-low)/2;
-            if(nums[mid] == target)
-                return  mid;
-            else if(nums[low]<=nums[mid]){
-                if(target>=nums[low] && target<nums[mid]){
-                    high = mid-1;
-                }else{
-                    low = mid+1;
+           
+            if(nums[mid] == target){
+                if(mid==low || nums[mid-1]!=target){
+                    return mid;
                 }
+                high = mid-1;
+            }else if(nums[mid]>target){
+                high = mid -1;
             }else{
-                if(target<=nums[high] && target>nums[mid]){
-                    low = mid +1;
-                }else{
-                    high = mid -1;
-                }
+                low = mid + 1;
             }
         }
+       
+        return -1;
+    }
+   
+        public static int traverseRight(int[] nums,int target){
+            int low = 0;
+            int high = nums.length-1;
+
+            while(low <= high){
+                int mid = low + (high-low)/2;
+
+                if(nums[mid] == target){
+                    if(mid==high || nums[mid+1]!=target){
+                        return mid;
+                    }
+                    low = mid+1;
+                }else if(nums[mid]>target){
+                    high = mid -1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+       
         return -1;
     }
 }

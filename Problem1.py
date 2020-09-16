396cc9dac3351dbe129328c6c7b5f21518ebaedc
siddhesh25 <siddheshyadav1@gmail.com>
2020-09-16 16:34:09 -0400

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,22 @@
+#Time Complexity: O(log n)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if nums[mid] > target and nums[left] <= target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target and nums[right] >= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1

09b764c1621b7972475c44a5888d9ef92311e17a
koshyanand <koshyanand@gmail.com>
2020-09-16 19:02:43 -0700

    Completed 2 as well

--- problem2_Search_in_Infinite_sorted_array.py
+++ problem2_Search_in_Infinite_sorted_array.py
@@ -1,28 +1,35 @@
 # """
-# // Time Complexity :O(log(N)) Since I have taken the multiplier as 2
-# // Space Complexity : O(log(N)) for the call stack
-# // Did this code successfully run on Leetcode : No
-# // Any problem you faced while coding this : Working though edge cases
+# // Time Complexity :O(log(T)) Index of the target value
+# // Space Complexity : O(log(T)) for the call stack
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : Working though edge cases and the stopping condition. Git it fixed through a hack
 
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
 
 class Solution:
     
     def find_target(self, reader, target, low, high):
-        # print(low, high)
-        if low >= high:
-            return -1
+
         
         val = reader.get(high)
-        # print(val)
         if val == target:
             return high
+        
+        if low >= high:
+            return -1
         elif val == self.invalid_output or val > target:
             mid = low + (high - low) // 2
+              
             return self.find_target(reader, target, low, mid)
 
         else:
             return self.find_target(reader, target, high, high * self.multiplier)
-
+        
     
     def search(self, reader, target):
         """
@@ -33,7 +40,9 @@ class Solution:
         self.invalid_output = 2147483647
         
         self.multiplier = 2
+
         
         return self.find_target(reader, target, 0, self.multiplier)
+        
         
         

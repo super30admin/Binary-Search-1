d7744f5996bfcc70345a0821469a8abd7543faca
Rajesh Thummala <52157903+rajeshthummala94@users.noreply.github.com>
2020-09-22 17:22:01 -0700

    changed time complexity

--- SortedArrayUnknownSize.java
+++ SortedArrayUnknownSize.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(1)
+// Time Complexity : O(logN)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : Yes

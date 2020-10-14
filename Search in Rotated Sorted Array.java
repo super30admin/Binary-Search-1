8dcf2ee5cdb11b7482018bbe1d2fb3de5796d5ed
Achyuth72 <68582633+Achyuth72@users.noreply.github.com>
2020-10-13 20:31:54 -0600

    Create  Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,51 @@
+// Time Complexity :O(logn)
+// Space Complexity :0(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : I have first implemented code using finding pivot, then I did the code using binary search Search in Rotated Sorted Array
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int lo=0;
+        int hi=nums.length-1;
+        int result=binarySearch(nums,lo,hi,target);
+          
+        return result;
+    }
+    public int binarySearch(int[] nums,int lo,int hi,int target)
+    {   
+         
+       
+        while(lo<=hi)
+        {
+        int mid=lo+(hi-lo)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        if(nums[lo]<=nums[mid])
+        {
+            if(target<nums[mid] && target>=nums[lo])
+            {
+                return binarySearch(nums,lo,mid,target);
+            }
+            else
+            {
+                 return binarySearch(nums,mid+1,hi,target);
+            }
+        }
+        else if(nums[mid]<=nums[hi])
+        {
+            if(target>nums[mid] && target<=nums[hi])
+            {
+                return binarySearch(nums,mid+1,hi,target);
+            }
+            else
+            {
+                return binarySearch(nums,lo,mid,target);
+            }
+        }
+        }
+        return -1;
+      
+    }
+}

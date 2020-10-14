07925f9240014e57eac728848eb23dd62c167b03
prachal80 <prachaljpatel@gmail.com>
2020-10-14 14:53:12 -0700

    Add files via upload

---
+++ searchRotatedArray.java
@@ -0,0 +1,36 @@
+// Time Complexity : o(logn)
+// Space Complexity : o(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        //edge case 
+        if(nums== null || nums.length == 0) return -1;
+        int low= 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            //check for the left part if it is sorted
+            else if(nums[low] <= nums[mid]){    
+                if(nums[mid] > target && nums[low]<= target){
+                    high = mid-1;
+                }else{
+                    low = mid+1; 
+                }                
+            }
+            //Otherwise, go for the right part if left is not sorted. Atleast one of them will be sorted
+            else{
+                if(nums[mid] < target && nums[high] >= target){
+                    low = mid+1;
+                }else{
+                    high = mid-1; 
+                } 
+            }
+        }
+    return -1;
+        }
+}

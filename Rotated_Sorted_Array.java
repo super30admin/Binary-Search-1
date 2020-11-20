dbd6926292f9e1d404e3629b65901859ec1d0cbd
Kavyath Basani <kavyathbasani@gmail.com>
2020-11-20 08:12:01 -0500

    Completed Binary search 1

--- Rotated_Sorted_Array.java
+++ Rotated_Sorted_Array.java
@@ -10,20 +10,35 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-    int start = 0, end = nums.length - 1;
-    while (start <= end) {
-      int mid = start + (end - start) / 2;
-      if (nums[mid] == target) return mid;
-      else if (nums[mid] >= nums[start]) {
-        if (target >= nums[start] && target < nums[mid]) end = mid - 1;
-        else start = mid + 1;
-      }
-      else {
-        if (target <= nums[end] && target > nums[mid]) start = mid + 1;
-        else end = mid - 1;
-      }
-    }
-    return -1;
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
         
+        int low = 0, high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }

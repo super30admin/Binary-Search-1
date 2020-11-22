aa126693d135f8c16267fff0f78d4ac7b82444f2
Henil Doshi <henildoshi1998@gmail.com>
2020-11-22 16:38:09 -0600

    added comments

--- BinarySearch1.py
+++ BinarySearch1.py
@@ -1,3 +1,10 @@
+# Time complexity: O(log(n))
+# Space Complexity: O(1) 
+# Approach: Binary search; if (mid == low or nums[mid-1] < nums[mid]) and (mid == high or nums[mid] > nums[mid+1]), we found the peak, return it's index.
+# Elseif nums[mid] > nums[mid+1], now we need to confirm for mid-1, so we move left (high = mid-1), else (nums[mid]<nums[mid+1]) we move right side.
+# Also nums[-1] = nums[n] = -∞, so we might get 0th or last element index as answer, as 1st elem < 0th elem > -∞ and last second elem < last elem > -∞
+# if no peak found, return -1
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 

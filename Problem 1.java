ce66e1db8cddc9222673c73980d2d1644fe74e07
Juhi Madhwani <jhmadhwa@ncsu.edu>
2020-12-23 14:59:35 -0500

    Completed Problem 1

---
+++ Problem 1.java
@@ -0,0 +1,22 @@
+class MatrixSearch
+{
+    public boolean searchMatrix(int[][] matrix, int target)
+    {
+        if(matrix.length == 0 || matrix[0].length == 0)
+            return false;
+        
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while(row < matrix.length && col >= 0)
+        {
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] < target)
+                row++;
+            else
+                col--;
+        }
+        return false;
+    }
+}

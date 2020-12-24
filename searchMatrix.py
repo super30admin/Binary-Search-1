0c18dcd36c097331aca39a43634b3607490c60a9
Soumyya Kanti Datta <61840898+skrantidatta@users.noreply.github.com>
2020-12-23 22:01:15 -0500

    please review

---
+++ searchMatrix.py
@@ -0,0 +1,31 @@
+#Time complexity = O(logm+logn) 
+#space complexity= O(1) 
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if(matrix==[] or matrix==[[]]):
+            return False
+        else:
+            l=0 
+            m=len(matrix)# no of rows in matrix
+            n=len(matrix[0])# no of columns in matrix
+            while(l<m): #searching the target in the 1st column , if not found the middle element will show the row in which the target may be present
+                middle_row=(l+m)//2
+                if(matrix[middle_row][0]==target):
+                    return True
+                elif(matrix[middle_row][0]>target):
+                    m=m-1
+                else:
+                    l=l+1
+            l=0
+            while(l<n):
+                middle_column=(l+n)//2
+                if(matrix[middle_row][middle_column]==target):
+                    return True
+                elif(matrix[middle_row][middle_column]>target):
+                    n=n-1
+                else:
+                    l=l+1
+            return False

560f396cedce0582935464aed398656f65b60ed4
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2020-12-24 16:06:32 -0600

    Working on it

---
+++ p3_search_unknown.py
@@ -0,0 +1,9 @@
+
+# working on it
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """

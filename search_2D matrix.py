0d5a9307c18e0c00f194cc1ffc4edcad6962c3a8
Anshul Atriek <atriekak@gmail.com>
2020-12-24 16:55:31 -0500

    Optimized code after class discussion

--- search_2D matrix.py
+++ search_2D matrix.py
@@ -1,35 +1,23 @@
-def binarySearchMatrix(matrix, l, r, target):
-    if r >= l:
-        mid = (l+r) // 2
-        if matrix[mid][0] <= target and matrix[mid][-1] >= target:
-            return mid
-        elif matrix[mid][0] > target:
-            return binarySearchMatrix(matrix, l, mid-1, target)
-        else:
-            return binarySearchMatrix(matrix, mid+1, r, target)
-    else:
-        return -1
-    
-def binarySearchArray(arr, l, r, target):
-    if r >= l:
-        mid = (l+r) // 2
-        if arr[mid] == target:
-            return True
-        elif arr[mid] > target:
-            return binarySearchArray(arr, l, mid-1, target)
-        else:
-            return binarySearchArray(arr, mid+1, r, target)
-    else:
-         return False       
-
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if len(matrix) == 0 or len(matrix[0]) == 0 or matrix[0][0] > target:
+        m = len(matrix)
+        if m == 0:
             return False
+        n = len(matrix[0])
         
-        row = binarySearchMatrix(matrix, 0, len(matrix)-1, target)
-        if row == -1:
-            return False
-        else:
-            return binarySearchArray(matrix[row], 0, len(matrix[0])-1, target)
+        l = 0
+        r = (m*n) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            print(mid)
+            row = mid // n
+            col = mid % n
+            
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
         
--- search_rotated sorted array.py
+++ search_rotated sorted array.py
@@ -1,21 +1,20 @@
-def binarySearch(arr, l, r, target):
-    if r >= l:
-        mid = (l+r) // 2
-        if arr[mid] == target:
-            return mid
-        if arr[l] <= arr[mid]: # left is sorted
-            if arr[mid] < target or arr[l] > target:
-                return binarySearch(arr, mid+1, r, target)
-            else:
-                return binarySearch(arr, l, mid-1, target)
-        else: # right is sorted
-            if arr[mid] > target or arr[r] < target:
-                return binarySearch(arr, l, mid-1, target)
-            else:
-                return binarySearch(arr, mid+1, r, target)
-    else:
-         return -1
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return binarySearch(nums, 0, len(nums)-1, target)
+        l = 0
+        r = len(nums) - 1
+        
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:      #left-side is sorted
+                if nums[l] <= target and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:                           #right-side is sorted
+                if nums[mid] < target and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1

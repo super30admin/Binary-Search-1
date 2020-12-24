a91bab0b74cfb2cd48fc02d4e748baf6053e439c
Soumyya Kanti Datta <61840898+skrantidatta@users.noreply.github.com>
2020-12-23 23:22:02 -0500

    please review

---
+++ Search in a Sorted Array of Unknown Size.py
@@ -0,0 +1,40 @@
+#Time complexity = O(logn) 
+#space complexity= O(1) 
+# Did this code successfully run on Leetcode :yes
+# Any problem you faced while coding this : no 
+
+
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#
+
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        l=0
+        r=1
+        nums=reader.get(r)
+        while(nums<target):
+            l=r+1
+            r=r*2
+            nums=reader.get(r)
+        
+        while(l<=r): #traversing through the array 
+            m=(l+r)//2 #searching for the middle element
+            nums=reader.get(m)
+            if(nums==target): 
+                return m
+            elif(nums>target):
+                r=m-1
+            else:
+                l=m+1
+        return -1

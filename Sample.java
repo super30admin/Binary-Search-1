f092d3d31295045738412d8e9a78b25a87deaeb6
pawan-gill <psgill@usc.edu>
2021-01-19 21:44:43 -0800

    Binary Search -1: search in array of unknown length

--- Sample.java
+++ Sample.java
@@ -8,49 +8,25 @@
 
 
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        int l = 0;
-        int r = m * n - 1;
-        int mid;
-        while (l <= r) {
-            mid = (l + r)/2;
-            int midElem = matrix[mid/n][mid%n];         //we will using / and % to traverse the matrix across rows
-            if (midElem == target)
-                return true;
-            else if (target < midElem)
-                r = mid - 1;
-            else  l = mid + 1;
+    public int search(ArrayReader reader, int target) {
+        int slow = 0;
+        int fast = 1;
+        while (reader.get(fast) < target) {
+            slow = fast;		// this acts like a sliding window where we will get a range where we can find the target
+            fast *= 2;
         }
-        return false;
+        while (slow <= fast) {
+            int mid = (slow + fast)/2;
+            if (reader.get(mid) == target)		//performing a binary search between indexes slow and fast
+                return mid;
+            else if (target < reader.get(mid))
+                fast = mid - 1;
+            else   slow = mid + 1;
+        }
+        
+        return -1;
     }
 }
-
-/**************** Another implementation ****************/
-
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-//         if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1])
-//             return false;
-//         int i = 0;
-//         for (i = 0; i < matrix.length - 1; i++)
-//             if (target >= matrix[i][0] && target < matrix[i + 1][0])
-//                 break;
-//         int low = 0;
-//         int high = matrix[0].length - 1;
-//         while (low <= high) {
-//             int mid = (low + high)/2;
-//             if (matrix[i][mid] == target)
-//                 return true;
-//             else if (target < matrix[i][mid])
-//                 high = mid - 1;
-//             else low = mid + 1;
-//         }
-//         return false;
-//     }
-// }
 
 
 

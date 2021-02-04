d6f406422e1f0cb6d23c0c9dffb8a1131ee72ef4
Juhi Madhwani <jhmadhwa@ncsu.edu>
2021-02-03 23:32:46 -0500

    Completed Problem 1

--- Problem 1.java
+++ Problem 1.java
@@ -26,3 +26,38 @@ class MatrixSearch
         return false;
     }
 }
+
+
+//Time Complexity: O(log(M*N))
+//Space Complexity: O(1)
+
+//Leetcode : 0 ms
+//Approach: Using binary search on imaginary flattened matrix and moving low and high pointers according to the mid value 
+//and converting the mid into corresponding row and column in the matrix using mid/n and mid%n.
+
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       if(matrix.length == 0 || matrix[0].length == 0)
+            return false;
+       int m = matrix.length;
+       int n = matrix[0].length;
+       int low = 0;
+       int high = m*n - 1;
+       
+       while(low <= high)
+       {
+           int mid = low + (high - low)/2;
+           int r = mid / n;
+           int c = mid % n;
+           if(matrix[r][c] == target)
+               return true;
+           else if(matrix[r][c] < target)
+               low = mid+1;
+           else
+               high = mid-1;
+       }
+       return false; 
+    }
+}

915733217bbb799722436fc3b9fc1b96c91a8cba
manideepkamarajugadda <manideepkamarajugadda@gmail.com>
2021-02-21 20:30:53 -0600

    Completed Sorted Array of Unknown size

--- ProblemRotatedArray.java
+++ ProblemRotatedArray.java
@@ -1,17 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int check=0;
+    int low = 0, high = nums.length - 1;
         
-        for(int i=0;i<nums.length;i++){
-            if(target==nums[i]){
-                check = i;
-                break;
-            }
-            else{
-                check = -1;
-            }
-        }  
-        return check;   
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target)
+          return mid;
+      else if (nums[mid] >= nums[low]) {
+        if (target >= nums[low] && target < nums[mid])
+            high = mid - 1;
+        else 
+            low = mid + 1;
+      }
+      else {
+        if (target <= nums[high] && target > nums[mid])
+            low = mid + 1;
+        else 
+            high = mid - 1;
+      }
     }
+    return -1;
+  }
+        
+        
 }
+
+//time complexity : O(logn)
+//Space complexity : O(1)

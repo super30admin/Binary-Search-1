1edfd559a472d1c90ef419cdc085002caa0b4d4c
MaryShilpaThumma <49599145+MaryShilpaThumma@users.noreply.github.com>
2021-02-24 00:06:53 -0500

    Create BinarySearchRoatatedSortedArray.java

---
+++ BinarySearchRoatatedSortedArray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
+        
+        while(low<=high){
+            mid = low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+                   }
+            else if (nums[low]<=nums[mid]){
+                if(target>=nums[low]&&target<nums[mid])
+                    high = mid-1;
+                     
+                else {
+                low = mid+1;
+               }}
+            else {
+                if(target<= nums[high]&&target > nums[mid]){
+              low = mid+1;
+                }
+                else
+                    high = mid-1;
+              }
+         
+        }
+        return -1;
+
+    }
+}

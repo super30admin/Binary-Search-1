b1102b2821e336cfedfdcff51da98ce9d0f1bd1f
Srikanth Reddy Nagidi <srikanth.nagidi@gmail.com>
2021-03-17 21:05:35 -0400

    added problem

---
+++ BinarySearch_Problem1.py
@@ -0,0 +1,34 @@
+#complexity - O(mlogn)
+# m - number of rows in matrix
+# n - number of columns in matrix
+def searchMatrix(matrix, target) -> bool:
+    for array in matrix:
+        if(array[len(array)-1] < target or array[0] > target):
+            continue
+        if(binarySearch(array, target)):
+            return True
+    return False
+
+#BinarySearch
+#complexity = O(log n)
+def binarySearch(array, target) -> bool:
+    left = 0
+    right = len(array) - 1
+    
+    while(left <= right):
+        mid = left + (right - left)//2
+        if(array[mid] == target):
+            return True
+        elif (array[mid] < target):
+            left = mid + 1
+        else:
+            right = mid - 1
+    return False
+        
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+target = 16
+target1 = 13
+
+print(searchMatrix(matrix, target))

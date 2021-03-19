01bbf7a2b8d622c991077fd1daea978bcf137414
Vishnu <46506049+vp43@users.noreply.github.com>
2021-03-18 19:09:58 -0500

    Completed Sprint Binary-Search-1

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,29 @@
+#binary search a 2D matrix
+''' Time complexity : O(log mn)
+Space Complexity: O(1)
 
+Did this code successfully run on Leetcode :Yes
+Any problem you faced while coding this :No'''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # get row and col numbers
+        m = len(matrix)
+        if m == 0:
+            return False
+        n = len(matrix[0]) 
+        # binary search
+        low, high = 0, (m * n) -1
+        while low <= high:
+                mid = (low + high) // 2
+                r = mid//n
+                c = mid%n
+                if matrix[r][c] == target:
+                    return True
+                elif target > matrix[r][c]:
+                        low = mid + 1
+                else:
+                    high = mid-1
+        return False
+        
+        
--- Problem3.py
+++ Problem3.py
@@ -1,1 +1,36 @@
+#Search in a Sorted Array of Unknown Size
 
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        low, high = 0, 1
+        if reader.get(0) == target:
+            return 0
+        while reader.get(high) < target:
+            low = high
+            high = 2*high
+         #binary search
+        while low <= high:
+            mid = low + (high - low)//2
+            x = reader.get(mid)
+            
+            if x == target:
+                return mid
+            if x < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        # there is no target element
+        return -1

6f715cad2ddc9aac28c5dbd60704fe523b2749e9
Yash Shah <yash2396@hotmail.com>
2021-03-21 15:57:24 +0530

    Completed Sprint BinarySearch1

--- Search_in_2D_Matrix.java
+++ Search_in_2D_Matrix.java
@@ -1,3 +1,11 @@
+
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int rows = matrix.length;

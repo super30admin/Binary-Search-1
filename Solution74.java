0712a0a757d7a63192350a29b92589f08e70c265
murdpra <79067522+murdpra@users.noreply.github.com>
2021-03-29 16:58:07 -0600

    Add files via upload

---
+++ Solution74.java
@@ -0,0 +1,34 @@
+class Solution74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if( matrix==null || matrix.length==0)
+        {
+            return false;
+        }
+        
+        int m=matrix.length;
+        int n=matrix[0].length;
+         int low=0, high=m*n-1;
+        
+        while(low<=high)
+        {
+        int mid= low+(high-low)/2;
+        
+        int r=mid/n;
+        int c=mid%n;
+        
+        if(matrix[r][c]==target)
+        {
+            return true;
+        }    
+    else if(target > matrix[r][c])
+    {
+        low=mid+1;
+    }
+        else
+        {
+            high=mid-1;
+        }}
+    return false;
+    }
+}

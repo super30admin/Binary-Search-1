c4b706a68f0a1d244fb16b23b8545a7268f8df64
Prajakta J <prajagj@gmail.com>
2021-04-23 13:43:28 -0500

    Update Problem1.java
    Comments added

--- Problem1.java
+++ Problem1.java
@@ -7,21 +7,24 @@
 // Any problem you faced while coding this :No
 
 
-
+//Used binary search approach
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+	//checking for edge case
        if(matrix.length==0)
             return false;
         int rows= matrix.length;
         int columns= matrix[0].length;
-
+	//left e
         int left=0;
         int right=rows*columns-1;
-
+	
+    
     while(left<=right){
+	//determine midpoint : numeric overflow case
         int midpoint=left+(right-left)/2;
         int mid_element=matrix[midpoint/columns][midpoint%columns];
-
+	//checking if target is equal to the mid element or greater and lesser than the mid element
         if(target==mid_element)
             return true;
         if(mid_element > target)
@@ -29,6 +32,7 @@ class Solution {
         if(mid_element < target) 
             left=midpoint+1;
         }
+	//case if the asked target isn't present
 	return false;
  }
 }

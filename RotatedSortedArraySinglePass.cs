f134e7f5e24ef2084007341a315e7fca77ad366c
tmujje <tmujje@gmail.com>
2021-04-24 02:54:24 -0400

    Completed Binary Search 1 assignment - Please review

---
+++ RotatedSortedArraySinglePass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : Initially when I tried this problem based on the algorithm
+    // discussed in the class - Identify the sorted side of an array and search the element on that side
+    // I missed to check if the right part of the array is sorted condition.It took a while for me to figure 
+    // out the mistake after a couple of failed runs on Leetcode
+    class RotatedSortedArraySinglePass
+    {
+        //Time Complexity - O(log n) - Since in worst case scenario we might be reducing search space on both sides.
+        //Space Complexity - O(1) - Since we are not using any additional space
+        public int Search(int[] nums, int target)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (nums[left] <= nums[mid])
+                { // Is the left part of the array sorted?
+                    if (target >= nums[left] && target < nums[mid])
+                    { // Is the target in the sorted side of the array
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else
+                { // Is the right part of the array sorted?
+                    if (target <= nums[right] && target > nums[mid])
+                    { // Is the target in the sorted side of the array
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ SearchMatrix.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithms
+{
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No. Even though the caluclation of Indices for the mid calculation was not intentional,
+    // it got clarified after going through the class video again.
+    public class SearchMatrix
+    {
+        //Time Complexity - O(log mn) since we are reducing the search space by half in 2D array while going through this imaginary flattened sorted array
+        //Space Complexity- O(1) since we are iterating within the given 2D array and we are not using any additional space
+        public bool Search(int[][] matrix, int target)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                return false;
+            }
+
+            var rows = matrix.Length;
+            var cols = matrix[0].Length;
+            var left = 0;
+            var right = rows * cols - 1;
+
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+                var row = mid / cols; // Caluclating row index for the mid element
+                var col = mid % cols; // Calculating column index of the mid element
+                if (matrix[row][col] == target) return true;
+                else if (matrix[row][col] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return false;
+        }
+    }
+}

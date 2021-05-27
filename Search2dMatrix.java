91724b5f885af21ac2fc421115052f8cf7f385ae
Yash Dhawan <84486109+yashdhawan-FSD@users.noreply.github.com>
2021-05-27 17:34:48 -0400

    Reverting the wrong commit
    The previous commit was for my personal use and was accidently pushed

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -1,9 +1,7 @@
 class Search2dMatrix {
-    public static boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
-        System.out.println(n);
         int m = matrix[0].length;
-        System.out.println(m);
         int col=m-1;
         int row =0;
         while(row<n && col >=0){
@@ -18,18 +16,5 @@ class Search2dMatrix {
             }
         }
         return false;
-    }
-
-    public static void main (String[] args)
-    {
-        int x = 80;
-        int mat[][] = {{0, 6, 8, 9, 11},
-                       {20, 22, 28, 29, 31},
-                       {36, 38, 50, 61, 63},
-                       {64, 66, 100, 122, 128}};
-     
-        Boolean v= searchMatrix(mat, x);
-        System.out.println(v);
-         
     }
 }

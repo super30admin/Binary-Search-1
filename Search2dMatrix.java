df9abe5603424814bd2c1dfb73385e5d59354230
Yash Dhawan <84486109+yashdhawan-FSD@users.noreply.github.com>
2021-05-27 17:29:53 -0400

    Solution to problem:01

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -1,7 +1,9 @@
 class Search2dMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
+        System.out.println(n);
         int m = matrix[0].length;
+        System.out.println(m);
         int col=m-1;
         int row =0;
         while(row<n && col >=0){
@@ -16,5 +18,18 @@ class Search2dMatrix {
             }
         }
         return false;
+    }
+
+    public static void main (String[] args)
+    {
+        int x = 80;
+        int mat[][] = {{0, 6, 8, 9, 11},
+                       {20, 22, 28, 29, 31},
+                       {36, 38, 50, 61, 63},
+                       {64, 66, 100, 122, 128}};
+     
+        Boolean v= searchMatrix(mat, x);
+        System.out.println(v);
+         
     }
 }

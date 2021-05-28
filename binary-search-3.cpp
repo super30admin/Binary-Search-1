0394717fdd516ea068ef367932c2ad632995f4d5
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-27 20:57:14 -0400

    Add files via upload

---
+++ binary-search-3.cpp
@@ -0,0 +1,33 @@
+int binarySearch(int arr[], int l, int r, int x)
+{
+    while(l<=r){
+        int mid = l + (r - l)/2;
+        if(arr[mid]== x){
+            return mid;
+        }
+        else if(arr[mid]>x){
+            r = mid-1;
+        }
+        else{
+            l = mid+1;   
+        }
+    }
+    return -1;
+}
+ 
+
+int findPos(int arr[], int key)
+{
+    int l = 0, h = 1;
+    int val = arr[0];
+ 
+    while (val < key)
+    {
+        l = h;        
+        h = 2*h;      
+        val = arr[h]; 
+    }
+ 
+    return binarySearch(arr, l, h, key);
+}
+ 

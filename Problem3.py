0dc87cd4a912870aeb2e1d6a9b10572a3cfb63b4
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-05-28 21:14:38 +0530

    Updated after class

--- Problem3.py
+++ Problem3.py
@@ -1,30 +1,18 @@
-# Time Complexity : O(logn base 100) + O(100)
-# Space Complexity : O(1)
+# Time Complexity : O(logn)
+# Space Complexity : O(logn)
 # Did this code successfully run on Leetcode : It's in premium so couldn't test it on leetcode,worked for my own testcases
 # Any problem you faced while coding this : No
 
 def unlimitedList(arr,target):
-    jump = 100
-    start = 0
-    exist = False
+    low = 0
+    high = 1
     while True:
-        try:
-            if arr[start+jump] == target:
-                return start+jump
-            elif arr[start+jump] < target:
-                start = start + jump
-            else:
-                exist = True
-                break
-
-        except:
-            exist = False
+        if arr[high] < target:
+            low = high
+            high = high*2
+        else:
             break
-    if exist:
-        return binarySearch(arr,start,start+jump,target)
-    else:
-        end = findLength(arr,start,start+jump)
-        return binarySearch(arr,start,end,target)
+    return binarySearch(arr,low,high)
 
 def binarySearch(arr,l,r,t):
     if l>r:
@@ -35,14 +23,4 @@ def binarySearch(arr,l,r,t):
     elif arr[mid] < t:
         return binarySearch(arr,mid+1,r,t)
     return binarySearch(arr,l,mid-1,t)
-
-def findLength(arr,start,end):
-    start = start + 1
-    while start <= end:
-        try:
-            a = arr[start]
-            start = start + 1
-        except:
-            return start
-    return end
 

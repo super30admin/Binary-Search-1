1a9177aca16de9e4d3c2c87d8aabdd62f6fdf399
krishnaattapu <krishna.attapu@gmail.com>
2021-05-28 00:38:01 -0500

    modified space complexity

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(log m + log n)
-// Space Complexity :O(n)
+// Space Complexity :O(m*n)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this : no
 

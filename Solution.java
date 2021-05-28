c25bf7449c5b32db8ac4489fe1a158884a4e5490
SuhasMC <chandrashekar.su@husky.neu.edu>
2021-05-27 21:12:41 -0400

    Attempted Binary-Search-1

--- Solution.java
+++ Solution.java
@@ -7,7 +7,7 @@ class Solution {
         int lo = 0;
         int hi = nums.length - 1;
         while (lo < hi) {
-            int mid = (lo + hi) / 2;
+            int mid = lo + (hi - lo) / 2;
             if (nums[mid] == target) return mid;
 
             if (nums[lo] <= nums[mid]) {

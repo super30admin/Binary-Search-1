3c2a92714667ec5ebcc58c237bc6e3e5005d82bb
SrinishRao <69674589+SrinishRao@users.noreply.github.com>
2021-05-30 07:54:13 +0530

    Completed Binary-Search-1 - Please Review

---
+++ Problem1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length; int n= matrix[0].length;
+        int low = 0; int high = m*n -1;
+        if(matrix == null || m ==0){
+            return false;
+        }
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int row = mid/n;
+            int col = mid%n;
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target) {
+                high = mid -1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}

39feb05ffd87b40c04d1b6f8111d82728f738cf3
HonglaiPeng <honglaipeng007@gmail.com>
2021-05-29 21:58:58 -0400

    Add files via upload

---
+++ LeetCode 33 - Search in Rotated Sorted Array.java
@@ -0,0 +1,34 @@
+// Time Complexity: Since we use binary search methods here, 
+// so the time complexity will be O(logn).
+/* --------------------------------------------------------*/
+// Space Complexity: the space complexity will be O(1).
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return -1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                return mid;
+            // check whether right sorted
+            }else if(nums[mid] >= nums[low]){
+                // check which interval the target locates.
+                if(nums[low] <= target && nums[mid] > target){
+                    high = mid -1;
+                }else{
+                    low = mid + 1;
+                }
+            // check whether left sorted
+            }else{
+                // check which interval the target locates.
+                if(nums[high] >= target && nums[mid] < target){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return  -1;
+    }
+}

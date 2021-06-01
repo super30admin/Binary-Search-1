7069d364d8a62988d53e6a355163947e706e4557
chaitanya003 <chaitanyakhinvasara33@gmail.com>
2021-06-01 08:03:16 +0530

    Added TC

--- search_in_sorted_array.cpp
+++ search_in_sorted_array.cpp
@@ -1,3 +1,7 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 class Solution {
 public:
     int search(vector<int>& nums, int target) 

2b7a0484133748cdb455aa8ce65a43863df7522e
SuhasMC <chandrashekar.su@husky.neu.edu>
2021-05-31 21:08:30 -0400

    Completed Binary-Search-1

--- BinarySearchMatrix.java
+++ BinarySearchMatrix.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 class BinarySearchMatrix {
     
     public boolean searchMatrix(int[][] matrix, int target) {

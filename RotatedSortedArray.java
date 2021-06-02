36549719e9d1b9661af38edc3040b353ac05c947
Darpit Chaudhary <chaudhary.da@husky.neu.edu:wq>
2021-06-02 03:00:57 -0400

    Review

---
+++ RotatedSortedArray.java
@@ -0,0 +1,33 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+class RotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if(nums==null || nums.length==0){
+            return -1;
+        }
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[low]<=nums[mid]){ //left side is sorted
+                if(nums[low]<=target && nums[mid]>target){ 
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }   
+            }else{ //right is sorted
+                if(nums[mid]<target && nums[high]>=target){ 
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }   
+            }
+        }
+        return -1;
+    }
+}

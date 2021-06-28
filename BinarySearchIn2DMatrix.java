6b879983869768d1e539bfa2d957487b3f785b41
Sreedharakurup <srerema@3c22fbd3c470.ant.amazon.com>
2021-06-28 08:00:06 -0500

    More Comments for ease of referencing later

--- BinarySearchIn2DMatrix.java
+++ BinarySearchIn2DMatrix.java
@@ -6,44 +6,51 @@
 
 // Your code here along with comments explaining your approach
 
-//The matrix is saved in memeory as 1 Dimensional array
-//JVM calculate the index of mid of matrix by formula row*n+column
-//We know where the midpoint is /we know the index, but we need to find the position in matrix where the index is.
-//For JVM, matrix [m][n] is the input and index is the output, but for us to code, we know the mid index by our eyes,but we need to find the corresponding row and column programmatically
-//Row calculation = mid index /noOfCoumns (Example: 5/4 =1)
-//COlumn Calculation = mid index % noOfColumns (Example: 5%4=1)
+//We are going to search the target in 2D matrix by binary search approach
+//Behing the hood/jvm,m*n matrix is stored as array list or 1D aray ,with first index reference to memory
+//For Binary search on array we need low,high and mid
+    //low is the first index
+    // high is the last index .How do we find it ?
+        // for that we need m(no of rows), n (no of columns) .so formula to find m(row) = matrix.length; formula to find n(column) = matrix[0].length
+    // mid index = low+(high-low)/2
+//Now how do we find the value of the corresponding mid index
+//JVM calculate the index for corresponding rowcolumn by  formula index = row*n+column.
+//Our challenge is to find the corresponding row column for the index
+    //Row calculation = mid index /noOfColumns (Example: 5/4 =1)
+    //COlumn Calculation = mid index % noOfColumns (Example: 5%4=1)
 //From above we derived the mid index position as [1][1]
 
-
-//assuming Flattening the matrix to list and find the mid index 
+//Summary
+//assuming Flattening the matrix to list and find the mid index
 // then do binary search
+
 
 
 class BinarySearchIn2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
+        //m*n matrix is stored as a array list in the backgroundby jvm
         if(matrix == null || matrix.length == 0)
-            return false;   
-            
-            //initilazing the matrix
-            int m = matrix.length, n = matrix[0].length;
-            int low = 0, high = m*n-1;
-            while(low <= high) {
-                //Calculating the mid
-                int mid = low + (high - low)/2;
-                //calculating row & column 
-                int row = mid/n;
-                int column = mid%n;
-                
-                if(matrix[row][column] == target)
-                    return true;
-                
-                else if(matrix[row][column] > target)
-                    high = mid-1;
-                else
-                    low = mid -1;
-                
-        }
             return false;
-    }
+
+        //initilazing the matrix
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m*n-1;
+        while(low <= high) {
+            //Calculating the mid index
+            int mid = low + (high - low)/2;
+            //calculating corresponding row & column of mid index
+            int row = mid/n;
+            int column = mid%n;
+
+            if(matrix[row][column] == target)
+                return true;
+
+            else if(matrix[row][column] > target)
+                high = mid-1;
+            else
+                low = mid -1;
+
+        }
+        return false;
 }
 
--- BinarySearchInRotatedSortedArray.java
+++ BinarySearchInRotatedSortedArray.java
@@ -5,27 +5,29 @@
 
 
 class BinarySearchInRotatedSortedArray {
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
         //null check and length zero
         if(nums == null || nums.length == 0)
             return -1;
         //calculate low & high
-            
-         int low = 0, high = nums.length-1;
-        
+
+        int low = 0, high = nums.length-1;
+
         //Binary Search
+        //Will come out of while loop when target is found or when Low gets shifted past the High/(meaning target not found in the array)
         while(low <= high){
             //if we do (low+high)/2, we might get iner overflow exception for some edge cases
             int mid = low+(high-low)/2;
             if(nums[mid] == target)
                 return mid;
-            
-    //in the rotated array ,you will a pattern that one side from mid id sorted and other is not
-    //To find which side is sorted- left or right?
-            
+
+            //in the rotated array ,you will a pattern that one side from mid is sorted and other is not
+            //We can do binary search on sorted array
+            //To find which side is sorted- left or right?
+
             //left side sorted
             if(nums[low] <=nums[mid]){
-         //check target on sorted side or target on unsorted side
+                //check target on sorted side or target on unsorted side or Comparing if target is within the low & the mid
                 if(nums[low] <= target && nums[mid] > target){
                     high = mid -1;
                 }else
@@ -34,16 +36,15 @@ class BinarySearchInRotatedSortedArray {
             //right side sorted
             else if(nums[mid] <= nums[high])
             {
-         //check target on sorted side or target on unsorted side
-            if(nums[mid] < target && nums[high] >=target){
-                low = mid +1;
+                //check target on sorted side or target on unsorted side or we can say comapring if target is within mid and high
+                if(nums[mid] < target && nums[high] >=target){
+                    low = mid +1;
+                }
+                else
+                    high = mid -1;
             }
-            else
-                high = mid -1;
         }
-        }
-        return -1;   
-    }
+        return -1;
 }
 
 

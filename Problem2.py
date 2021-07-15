041d6086ad83ff0601ae17565a4ed9261e6735cd
Meghna <meghudps@gmail.com>
2021-07-14 20:37:32 -0400

    Correcting problem2

--- Problem2.py
+++ Problem2.py
@@ -1,10 +1,7 @@
 
 ## Problem2
-# Search in Infinite sorted array: 
+# Search in a Rotated Sorted Array (https://leetcode.com/problems/search-in-rotated-sorted-array/)
 
-# https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
-
-# Given a sorted array of unknown length and a number to search for, return the index of the number in the array. Accessing an element out of bounds throws exception. If the number occurs multiple times, return the index of any occurrence. If it isn’t present, return -1.
 
 
 class Solution:

ece0e27c3f8c7afe71f03785093dfcbfb7616164
Aparna <aparna20dalal@gmail.com>
2021-07-22 01:51:33 -0500

    Problem#5 submission

---
+++ Problem#5_Search_In_a_2D_Matrix.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+      // Solution Binary search using a pivot element
+      int r = matrix.length;
+      if(r==0) return false;
+      int c = matrix[0].length;
+     
+      //BST select/find out the pivot/mid index
+      int left = 0;
+      int right = r*c-1; //Total elements if linear array considered
+      int pivot, pivotVal;
+      while(left<=right){
+         pivot = (left+right)/2;
+         pivotVal = matrix[pivot/c][pivot%c];
+          if(pivotVal==target) return true;
+          else{
+              if(target>pivotVal){
+            	  left = pivot + 1;
+              }
+             } else{
+                 right = pivot -1;
+              }
+         }
+     }
+     return false;
+    	
+//        //Solution - Bruteforce
+//        if(matrix.length==0||matrix.length==0) return false;
+//        int col1 = matrix[0].length-1;
+//        int row1 = 0;
+//        
+//        while(row1<matrix.length && col1>=0)
+//        {
+//            if(matrix[row1][col1]>target) col1--;
+//            else if(matrix[row1][col1]<target) row1++;
+//            else return true;
+//        }
+//        return false;           
+        
+    }
+}

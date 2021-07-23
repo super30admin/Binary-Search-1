36bf384e0a0aeef0131385fe828fa7a0bb9a0b15
VishLivecoder <vxv190045@utdallas.edu>
2021-07-23 11:26:24 -0500

    Completed both problems

--- Sample.java
+++ Sample.java
@@ -8,48 +8,71 @@
 
 class Solution {
     public int search(int[] nums, int target) {
-        
-        //firstly I am doing a binary search to find the smallest element in the array.
-        
-        int first=0;
-        int last=nums.length-1;
+        int low=0;
+        int high=nums.length-1;
         int mid=0;
-        
-        while(first<last){
-            mid=(first+last)/2;
-            if(nums[mid]>nums[last]){
-                first=mid+1;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-            else{
-                last=mid;
-            }
-        }
-        
-        int start=first;
-        first=0;
-        last=nums.length-1;
-        
-        if(target>=nums[start] && target<=nums[last]){
-            first=start;
-        }else{
-            last=start-1;
-        }
-        
-        while(first<=last){
-            mid=(first+last)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]<target){
-                first=mid+1;
-            }else{
-                last=mid-1;
+            if(nums[low]<=nums[mid] ) {
+                if(target>=nums[low] && target<nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+                
             }
             
+            else{
+                if(target>nums[mid] && target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
         }
         return -1;
-        
-        
         
     }
 }
 
+// Time Complexity :O(logn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :nope
 
+
+// Your code here along with comments explaining your approach
+
+
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0){
+            return false;
+        }
+        int low=0;
+        int high=matrix[0].length*matrix.length-1;
+        int row=0;
+        int col=0;
+        int mid=0;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            row=mid/matrix[0].length;
+            col=mid%matrix[0].length;
+            if(matrix[row][col]==target){
+                return true;
+            }if(matrix[row][col]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+            
+        }
+        return false;
+        
+    }
+}

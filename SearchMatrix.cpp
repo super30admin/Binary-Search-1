160c2c6943ca36fc3daa6f00799744cd1e5142b1
ShanmathiS <59035995+ShanmathiS@users.noreply.github.com>
2021-09-05 17:47:15 -0700

    Update SearchMatrix.cpp

--- SearchMatrix.cpp
+++ SearchMatrix.cpp
@@ -1,4 +1,4 @@
-// Time Complexity : O(M) // M is size of each row
+// Time Complexity : O(MN) 
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
@@ -7,21 +7,19 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if (matrix.size() == 0) return false;
-        int i = 0;
-        while(i < matrix.size() - 1 && matrix[i + 1][0] <= target)
-        {
-            i++;
-            if(matrix[i][0] == target) return true;
-        }
-        
+        int row = 0, col = 0;
         int left = 0;
-        int right = matrix[0].size() - 1;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int right = (m * n) - 1;
         
         while (left <= right)
         {
             int mid = left + (right - left) / 2;
-            if (matrix[i][mid] == target) return true;
-            else if (matrix[i][mid] > target) right = mid - 1;
+            row = mid / n;
+            col = mid % n;
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] > target) right = mid - 1;
             else left = mid + 1;
         }
         return false;

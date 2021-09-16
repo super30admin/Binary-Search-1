beddecc522a70a2325bb4b03a5211562cf1ec60a
rushilshah121198 <86422497+rushilshah121198@users.noreply.github.com>
2021-09-15 23:48:44 -0400

    completed #5.

---
+++ prob6.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+
+        if(nums.length == 0){
+            return -1;
+        }
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            else if(nums[mid] >= nums[start]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid -1;
+                }
+                else
+                {
+                    start = mid +1;
+                }
+            }
+
+            else{
+                if(target >= nums[mid] && target <= nums[end]){
+                    start = mid + 1;
+                }
+                else {
+                    end = mid -1;
+                }
+
+            }
+
+
+        } return -1;
+
+    }
+}

c02240a31507d4bfb420e3165b9d261765b8a9e9
Shradha Pawar <7shradhapawar@gmail.com>
2021-09-19 15:56:49 -0700

    Completed Binary-Search-1

---
+++ search-rotated-array.java
@@ -0,0 +1,33 @@
+/ Time Complexity : O(log n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+    int minIdx = findMinIdx(nums);
+    if (target == nums[minIdx]) return minIdx;
+    int m = nums.length;
+    int start = (target <= nums[m - 1]) ? minIdx : 0;
+    int end = (target > nums[m - 1]) ? minIdx : m - 1;
+    
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid]) start = mid + 1;
+        else end = mid - 1;
+    }
+    return -1;
+}
+
+public int findMinIdx(int[] nums) {
+    int start = 0, end = nums.length - 1;
+    while (start < end) {
+        int mid = start + (end -  start) / 2;
+        if (nums[mid] > nums[end]) start = mid + 1;
+        else end = mid;
+    }
+	return start;
+}
+}

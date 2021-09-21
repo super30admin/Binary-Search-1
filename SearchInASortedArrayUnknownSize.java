7f4ece73fdb8edb78fb58135a9fcd4491025b331
Parth Patel <patelparth622@gmail.com>
2021-09-20 21:40:32 -0700

    completed binary search

---
+++ SearchInASortedArrayUnknownSize.java
@@ -0,0 +1,31 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public  int search(ArrayReader reader, int target) {
+    	// we dont know size of array so we will assume highest integer possible values
+        int l =0;
+        int h = Integer.MAX_VALUE;
+        
+        while(h>= l){
+            
+            int mid = l +( h-l)/2;
+            //using given function to get element of secret array
+            int midElement = reader.get(mid);
+            
+            if(midElement == target){
+                return mid;
+            } else if (target < midElement ) {
+                h = mid -1;
+            } else {
+                l = mid +1;
+            }
+        }
+        
+        return -1;
+    }
+}
---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,44 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        //Inti low and high for indexes
+        int high = nums.length -1;
+        int low=0;
+        
+        while(high >= low){
+            // calculated mid
+            int mid = low + ( high - low) / 2;
+            
+            if(nums[mid] == target){
+                return mid;
+            } 
+            //Checking if left side of mid is sorted 
+            else if(nums[mid]>=nums[low])
+            {  // if target into sorted small array range will update low and high accordingly
+                if(nums[low] <= target &&  nums[mid] > target){
+                    high = mid -1;
+                } else {
+                    low = mid +1;
+                    
+                }
+            } else {
+            	// if target into sorted small array range will update low and high accordingly
+
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid+1;
+                }else {
+                    high = mid -1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}

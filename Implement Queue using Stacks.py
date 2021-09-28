f91d9bb9550a9082949b828558c60f8b517e9248
1337coder2021 <90594228+1337coder2021@users.noreply.github.com>
2021-09-27 17:36:58 -0700

    with time and space complexity

---
+++ Implement Queue using Stacks.py
@@ -0,0 +1,49 @@
+# Implement Queue using Stacks
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+        
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        ele = self.peek()
+        self.stack.remove(ele)
+        return ele
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if not self.stack:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()

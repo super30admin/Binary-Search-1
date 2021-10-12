21c56e940ed539bd2ed58cf08e4a4329603974aa
likhitha-333 <likhitha@bu.edu>
2021-10-12 16:27:50 -0400

    BinarySearch-1 Completed

---
+++ SearchRotatedSortedArray.java
@@ -0,0 +1,50 @@
+// Time Complexity :O(logn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        int mid;
+        
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            
+            if(nums[start]<=nums[mid])
+            {
+                if(target<nums[mid] && target>= nums[start])
+                {
+                    end=mid-1;
+                }
+                
+                else
+                {
+                    start=mid+1;
+                }
+            }
+            
+            else 
+            {
+                if(target<=nums[end] && target>nums[mid])
+                {
+                    start=mid+1;
+                }
+                else
+                {
+                    end=mid-1;
+                }
+            }
+        }
+        
+        return -1;
+        
+    }
+}

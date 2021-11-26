c3a303e28054adaa31abe8843decf2874a289d5b
Ashish Kumar <kumar.as@husky.neu.edu>
2021-11-25 23:22:31 -0600

    modified probem1

--- problem1-1.py
+++ problem1-1.py
@@ -10,10 +10,9 @@
 
 #Binary Search Approach
 
-
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+       
             def bin_search(arr: List[int], num: int) -> int:
                 l, r = 0, len(arr) - 1
                 while l + 1 < r:
@@ -24,9 +23,11 @@ class Solution:
                         l = mid
                 return l, r
             
-        row1, row2 = bin_search([matrix[i][0] for i in range(len(matrix))], target)
-        col1, col2 = bin_search(matrix[row1], target)
-        if matrix[row1][col1] == target or matrix[row1][col2] == target:
-            return True
-        col1, col2 = bin_search(matrix[row2], target)
-        return matrix[row2][col1] == target or matrix[row2][col2] == target
+            row1, row2 = bin_search([matrix[i][0] for i in range(len(matrix))], target)
+            col1, col2 = bin_search(matrix[row1], target)
+            print(matrix[row1][col1])
+            print(matrix[row2][col2])
+            if matrix[row1][col1] == target or matrix[row1][col2] == target:
+                return True
+            col1, col2 = bin_search(matrix[row2], target)
+            return matrix[row2][col1] == target or matrix[row2][col2] == target
---
+++ problem3.py
@@ -0,0 +1,30 @@
+# This is an interactive problem.
+
+# You have a sorted array of unique elements and an unknown size. You do not have an access to the array but you can use the ArrayReader interface to access it. You can call ArrayReader.get(i) that:
+
+# returns the value at the ith index (0-indexed) of the secret array (i.e., secret[i]), or
+# returns 231 - 1 if the i is out of the boundary of the array.
+# You are also given an integer target.
+
+# Return the index k of the hidden array where secret[k] == target or return -1 otherwise.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+
+# Example 1:
+
+# Input: secret = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in secret and its index is 4.
+# Example 2:
+
+# Input: secret = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in secret so return -1.
+
+
+# Constraints:
+
+# 1 <= secret.length <= 104
+# -104 <= secret[i], target <= 104
+# secret is sorted in a strictly increasing order.

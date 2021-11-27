72f59ec4371689046a5d96defb97e7ccbecf80a8
Manpreet Singh <mn407919@dal.ca>
2021-11-26 23:28:51 -0800

    Binary-Search-1 completed Please review

---
+++ SearchRoataedSortedArray.java
@@ -0,0 +1,47 @@
+
+// time complexity = O(logm)
+//space : O(1)
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        
+        int low = 0;
+        int high = nums.length -1;
+        
+        while(low<=high)
+        {
+            
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            
+            if(nums[mid] >= nums[low] )
+            {
+                if(nums[mid]>=target && target >= nums[low]  )
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            
+            else 
+            {
+                if(target>=nums[mid] && target <=nums[high])
+                {
+                    low = mid +1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}

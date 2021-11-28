3594de7a139f4eb2079b80ae4a3e1a87c9e739f5
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-11-27 20:22:43 -0800

    Search in Rotated Sorted Array

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // Before this we implemented this using linear search of the index and then
-        // binary search of the row
-        // For doubt check s30 video
-        int low = 0;
-        int mid = 0;
-        int numOfRows = matrix.size();
-        int numOfColumns = matrix[0].size();
-        int row = 0;
-        int column = 0;
-        int high = (numOfRows * numOfColumns) -1;
+    int search(vector<int>& nums, int target) {
+        int low = 0;//0
+        int high = nums.size()-1;//2
+        int mid = 0;//
+
         while(low <= high){
-            mid = (low + high)/2;
-            row = mid/numOfColumns;
-            column = mid % numOfColumns;
-            if(matrix[row][column] == target){
-                return true;
-            }else if(matrix[row][column] > target){
-                high = mid-1;
-            }else if(matrix[row][column] < target){
-                low = mid +1;
+            mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] > target){
+                    high = mid -1;
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]< target && nums[high] >= target){
+                    low = mid+1;
+                }else{
+                    high = mid -1;
+                }
             }
         }
-        return false;
+        return -1;
     }
 };

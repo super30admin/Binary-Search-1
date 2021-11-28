a4c46e8199a19bf9c33fb8ea6d1cd804556894d0
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-11-27 20:21:15 -0800

    Search a 2D Matrix

--- main.cpp
+++ main.cpp
@@ -1,31 +1,28 @@
-/**
- * // This is the ArrayReader's API interface.
- * // You should not implement it, or speculate about its implementation
- * class ArrayReader {
- *   public:
- *     int get(int index);
- * };
- */
-
 class Solution {
 public:
-    int search(const ArrayReader& reader, int target) {
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // Before this we implemented this using linear search of the index and then
+        // binary search of the row
+        // For doubt check s30 video
         int low = 0;
-        int high = 1;
-        while(reader.get(high) < target){
-            low = high;
-            high = high * 2;
-        }
+        int mid = 0;
+        int numOfRows = matrix.size();
+        int numOfColumns = matrix[0].size();
+        int row = 0;
+        int column = 0;
+        int high = (numOfRows * numOfColumns) -1;
         while(low <= high){
-            int mid = low  + (high - low)/2;
-            if(reader.get(mid) == target){
-                return mid;
-            }else if(reader.get(mid)> target){
-                high = mid -1;
-            }else if(reader.get(mid) < target){
+            mid = (low + high)/2;
+            row = mid/numOfColumns;
+            column = mid % numOfColumns;
+            if(matrix[row][column] == target){
+                return true;
+            }else if(matrix[row][column] > target){
+                high = mid-1;
+            }else if(matrix[row][column] < target){
                 low = mid +1;
             }
         }
-        return -1;
+        return false;
     }
 };

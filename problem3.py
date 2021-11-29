6f932587cf5c51cd5a04e458fac7ec8ee21746e7
Ashish Kumar <kumar.as@husky.neu.edu>
2021-11-29 13:47:51 -0600

    chnaged the problem 3

--- problem3.py
+++ problem3.py
@@ -1,5 +1,41 @@
-# This is an interactive problem.
 
+# Time Complexity : O(log(T))  T is index of target
+# Space Complexity : 0(1)
+
+# Did this code successfully run on Leetcode : 
+# cannot check 
+
+# Any problem you faced while coding this : 
+
+# Your code here along with comments explaining your approach
+
+#Binary Search Approach
+
+
+# Leetcode problem ( shared because it is not accessible in leetcode without subscription )
+
+class Solution:
+    def search(self, reader, target):
+        l=0
+        r = 1
+        while reader.get(r) <target:
+            #l = r  This does not work
+            r *=2
+        if reader.get(l) == target:
+            return l
+        if reader.get(r) == target:
+            return r        
+        while l+1 < r:
+            mid = l+(r-l)//2
+            if reader.get(mid) == target:
+                return mid
+            elif reader.get(mid) <target:
+                l = mid
+            else :
+                r = mid    
+
+
+'''
 # You have a sorted array of unique elements and an unknown size. You do not have an access to the array but you can use the ArrayReader interface to access it. You can call ArrayReader.get(i) that:
 
 # returns the value at the ith index (0-indexed) of the secret array (i.e., secret[i]), or
@@ -28,3 +64,4 @@
 # 1 <= secret.length <= 104
 # -104 <= secret[i], target <= 104
 # secret is sorted in a strictly increasing order.
+'''

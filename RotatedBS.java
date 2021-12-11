917f813db98ffa0550d3c3a458bfb0d62f22cae8
Taranjit Kaur <taranjit@chegg.com>
2021-12-11 21:12:35 +0530

    Binary Search and SearchIn2D array.

--- RotatedBS.java
+++ RotatedBS.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(logn)
+// Space Complexity : O(logn) for recursive calls
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 class RotatedBS {
     public int search(int[] nums, int target) {
 
@@ -14,15 +19,23 @@ class RotatedBS {
 
         int mid = start +(end-start)/2;
 
+        // If element is found, return the index.
         if(nums[mid] == target) {
             return mid;
-        } else if(nums[mid] >= nums[start]) {
+        }
+        // else check if the array is sorted in the first half
+        else if(nums[mid] >= nums[start]) {
+            // we see if the target is in first half range, then search in it
+            // else search in second half
             if(target <= nums[mid]  &&  nums[start] <= target) {
                 return helper(nums, target, start, mid-1);
             } else {
                 return helper(nums, target, mid+1, end);
             }
-        } else {
+        }
+        // If array is not sorted in first half, its worth searching in both the halves
+        // for the test case like this, [5 1 3] target = 5
+        else {
             int ans = helper(nums, target, start, mid-1);
             return ans ==-1 ? helper(nums, target, mid+1, end) : ans;
         }
--- SearchIn2D.java
+++ SearchIn2D.java
@@ -1,101 +1,97 @@
-// Time Complexity : O(logm*logn)
-// Space Complexity : O(1)
-// Did this code successfully run on Leetcode : Y
+// Time Complexity : O(log(mn))
+// Space Complexity : Recursive calls taken the space - could be O(log(mn)) - not sure, please correct
+// Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : Yes, while setting the constraints that lead to array our of bounds exception
+// Facing problems while calculating the time complexity and space complexity
 
-
-// Your code here along with comments explaining your approach
 
 public class SearchIn2D {
 
-    class Solution {
-        public boolean searchMatrix(int[][] matrix, int target) {
-            int m = matrix.length;
-            int n = matrix[0].length;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-            if(m==1 && n==1) {
-                return matrix[0][0] == target;
-            }
-
-            if(m == 1) {
-                int ans = binarySearch(matrix[0], target, 0, n-1);
-                return ans != -1;
-            }
-            return  helper(matrix, target, 0, m-1, 0, n-1);
+        if(m==1 && n==1) {
+            return matrix[0][0] == target;
         }
 
-        public boolean helper(int [][] matrix, int target, int rStart, int rEnd, int cStart, int cEnd) {
-
-            if(rStart > rEnd || cStart > cEnd || rStart <0 || cStart<0 || rEnd >= matrix.length || cEnd >=matrix[0].length) {
-                return false;
-            }
-
-            int rowMid = rStart + (rEnd -rStart)/2;
-            int cMid = cStart + (cEnd -cStart)/2;
-
-            if(matrix[rowMid][cMid] == target) {
-                return true;
-            }
-
-            // Reaching here when only two rows are left, and in one of the row we might have the target present
-            if(rEnd - rStart < 2) {
-
-                // if the target is > mid, in that case we apply binary search over two arrays
-                // option1 - in the row below the mid row, because the target is greater than mid
-                // If we don't find ans in the first option 1, we search in the same row but the col start is greater than the col mid.
-                if(matrix[rowMid][cMid] < target) {
-                    int ans1 = binarySearch(matrix[rowMid], target, cMid+1, cEnd);
-                    if(ans1 ==-1) {
-                        int ans2 = binarySearch(matrix[rowMid+1], target, cStart, cEnd);
-                        return ans2 != -1;
-                    } else {
-                        return true;
-                    }
-                }
-                // This is just the reverse of the above case
-                // We are trying to find the ans in the above row because target < mid element.
-                else {
-                    int ans1 = binarySearch(matrix[rowMid], target, cStart, cMid-1);
-                    if(ans1 ==-1) {
-                        int ans2 = binarySearch(matrix[rowMid-1], target, cStart, cEnd);
-                        return ans2 != -1;
-                    } else {
-                        return true;
-                    }
-                }
-
-            }
-
-            // We are reducing the search space by rows
-            // We are doing this till only two rows are left
-            if(matrix[rowMid][cMid] < target) {
-                return helper(matrix, target, rowMid, rEnd, cStart, cEnd);
-            } else {
-                return helper(matrix, target, rStart, rowMid, cStart, cEnd);
-            }
-
+        if(m == 1) {
+            int ans = binarySearch(matrix[0], target, 0, n-1);
+            return ans != -1;
         }
-
-        //Normal Binary Search method that returns the index when target is found
-        int binarySearch(int [] arr, int target, int startIndex, int endIndex) {
-
-            int midIndex = startIndex + (endIndex - startIndex) / 2;
-
-            while (startIndex < endIndex) {
-                if(arr[midIndex] == target) {
-                    return midIndex;
-                }
-
-                if(arr[midIndex] < target) {
-                    startIndex = midIndex +1;
-                } else {
-                    endIndex = midIndex -1;
-                }
-                midIndex = startIndex + (endIndex - startIndex) / 2;
-            }
-            return -1;
-        }
+        return  helper(matrix, target, 0, m-1, 0, n-1);
     }
 
+    public boolean helper(int [][] matrix, int target, int rStart, int rEnd, int cStart, int cEnd) {
 
+        if(rStart > rEnd || cStart > cEnd || rStart <0 || cStart<0 || rEnd >= matrix.length || cEnd >=matrix[0].length) {
+            return false;
+        }
+
+        int rowMid = rStart + (rEnd -rStart)/2;
+        int cMid = cStart + (cEnd -cStart)/2;
+
+        if(matrix[rowMid][cMid] == target) {
+            return true;
+        }
+
+        // Reaching here when only two rows are left, and in one of the row we might have the target present
+        if(rEnd - rStart < 2) {
+
+            // if the target is > mid, in that case we apply binary search over two arrays
+            // option1 - in the row below the mid row, because the target is greater than mid
+            // If we don't find ans in the first option 1, we search in the same row but the col start is greater than the col mid.
+            if(matrix[rowMid][cMid] < target) {
+                int ans1 = binarySearch(matrix[rowMid], target, cMid+1, cEnd);
+                if(ans1 ==-1) {
+                    int ans2 = binarySearch(matrix[rowMid+1], target, cStart, cEnd);
+                    return ans2 != -1;
+                } else {
+                    return true;
+                }
+            }
+            // This is just the reverse of the above case
+            // We are trying to find the ans in the above row because target < mid element.
+            else {
+                int ans1 = binarySearch(matrix[rowMid], target, cStart, cMid-1);
+                if(ans1 ==-1) {
+                    int ans2 = binarySearch(matrix[rowMid-1], target, cStart, cEnd);
+                    return ans2 != -1;
+                } else {
+                    return true;
+                }
+            }
+
+        }
+
+        // We are reducing the search space by rows
+        // We are doing this till only two rows are left
+        if(matrix[rowMid][cMid] < target) {
+            return helper(matrix, target, rowMid, rEnd, cStart, cEnd);
+        } else {
+            return helper(matrix, target, rStart, rowMid, cStart, cEnd);
+        }
+
+    }
+
+    //Normal Binary Search method that returns the index when target is found
+    int binarySearch(int [] arr, int target, int startIndex, int endIndex) {
+
+        int midIndex = startIndex + (endIndex - startIndex) / 2;
+
+        while (startIndex < endIndex) {
+            if(arr[midIndex] == target) {
+                return midIndex;
+            }
+
+            if(arr[midIndex] < target) {
+                startIndex = midIndex +1;
+            } else {
+                endIndex = midIndex -1;
+            }
+            midIndex = startIndex + (endIndex - startIndex) / 2;
+        }
+        return -1;
+    }
 }
+

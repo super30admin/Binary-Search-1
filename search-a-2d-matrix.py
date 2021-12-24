ccf81f03fe175f6a95d02ecd28345cad28288ffb
Jayavardhan Karampudi <jay@Jais-MacBook-Pro.local>
2021-12-24 11:21:13 -0700

    Optimized Code

--- search-a-2d-matrix.py
+++ search-a-2d-matrix.py
@@ -5,23 +5,47 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        def binarySearch(target, row):
-            left, right = 0, len(row)-1
-            while left <= right:
-                mid = (left + right)//2
-                if row[mid] == target: return True
-                elif row[mid] < target: left = mid + 1
-                else: right = mid - 1
-            return False
 
-        def arrSearch(target, search_arr):
-            left, right = 0, len(search_arr)-1
-            while left <= right:
-                mid = (left+right)//2
-                if search_arr[mid] <= target: left = mid+1
-                else: right = mid-1
-            return right
+# Time Complexity: O(log(mn))
+        def convert1d2d(val):
+            n = len(matrix[0])
+            m = len(matrix)
+            return val//n, val%n
 
-        # search_arr = [row[0] for row in matrix]
-        # searcher_index = arrSearch(target, search_arr)
-        return binarySearch(target, matrix[arrSearch(target, [row[0] for row in matrix])])
+        if not matrix: return False
+        low = 0
+        n = len(matrix[0])
+        m = len(matrix)
+        high = (m*n)-1
+        while (low <= high):
+            mid = (low+high)//2
+            row, col = convert1d2d(mid)
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+
+# TimeComplexity: O(logm+logn)
+#         def binarySearch(target, row):
+#             left, right = 0, len(row)-1
+#             while left <= right:
+#                 mid = (left + right)//2
+#                 if row[mid] == target: return True
+#                 elif row[mid] < target: left = mid + 1
+#                 else: right = mid - 1
+#             return False
+
+#         def arrSearch(target, search_arr):
+#             left, right = 0, len(search_arr)-1
+#             while left <= right:
+#                 mid = (left+right)//2
+#                 if search_arr[mid] <= target: left = mid+1
+#                 else: right = mid-1
+#             return right
+
+#         # search_arr = [row[0] for row in matrix]
+#         # searcher_index = arrSearch(target, search_arr)
+#         return binarySearch(target, matrix[arrSearch(target, [row[0] for row in matrix])])
--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -5,32 +5,59 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        def binarySearch(target, row):
-            left, right = 0, len(row)-1
-            while left <= right:
-                mid = (left + right)//2
-                if row[mid] == target: return mid
-                elif row[mid] < target: left = mid + 1
-                else: right = mid - 1
-            return -1
-
-        p1, p2, temp = 0, 1, -1
-        size = len(nums)
-        while p2 <= size-1:
-            if nums[p1] > nums[p2]:
-                temp = p2
-                break
-            p1 = p2
-            p2 += 1
         
-        if temp == -1:
-            return binarySearch(target, nums)
-        else:
-            xx = binarySearch(target, nums[:temp])
-            yy = binarySearch(target, nums[temp:])
-            if xx != -1:
-                return xx
-            elif yy != -1:
-                return temp+yy
-            else:
-                return -1
+
+#Optimized
+        low = 0
+        high = len(nums) - 1
+        while (low <= high):
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[low]: # Checking if its left sorted
+                if nums[mid] > target and nums[low] <= target: # Checking if it lies in the sorted side
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else: # Checking if its right sorted
+                if nums[mid] < target and nums[high] >= target: # Checking if it lies in the sorted side
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+#BruteForce
+#         def binarySearch(left, right):
+#             while left <= right:
+#                 mid = (left + right)//2
+#                 if nums[mid] == target:
+#                     return mid
+#                 elif nums[mid] < target:
+#                     left = mid + 1
+#                 else:
+#                     right = mid - 1
+
+#             return -1
+
+#         p1, p2, temp, size = 0, 1, -1, len(nums)
+#         left, right = 0, size-1
+#         while p2 <= size-1:
+#             if nums[p1] > nums[p2]:
+#                 temp = p2
+#                 break
+#             p1 = p2
+#             p2 += 1
+        
+#         if temp == -1:
+#             return binarySearch(left, right)
+#         else:
+#             if nums[temp] == target:
+#                 return temp
+#             elif nums[temp] < target and nums[right] >= target:
+#                 left = temp + 1
+#             else:
+#                 right = temp - 1
+#             return binarySearch(left, right)
+
+
+

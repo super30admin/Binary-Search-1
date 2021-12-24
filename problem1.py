6c728401743d9b8b4a9c98fede0317068b670db0
nikhil <mandgenikhil@gmail.com>
2021-12-23 21:18:28 -0500

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,14 @@
+xclass Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+    
+        for x in range(len(nums)):
+            if nums[x] == target:
+                return x
+        return -1
+                
+            
+            
+            
+            
+     

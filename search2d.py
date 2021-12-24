407e932e444e12529d621ed0ab05d2dac31f19cf
LiberoPat <patrick.trias@gmail.com>
2021-12-23 22:39:49 -0800

    search2d in logn

--- search2d.py
+++ search2d.py
@@ -1,6 +1,4 @@
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 
-print(len(matrix))
 """
 Use binary search algo
 refer to middle, compare to target
@@ -8,14 +6,19 @@ refer to middle, compare to target
 
 
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    def __init__(self):
+        print("in init")
+
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+
+
         #perform 2 binary search, runtime is O(log(mn)
         if len(matrix[0]) == 0 or len(matrix) == 0:
             return False
 
         low = 0; high = len(matrix)
         #binary search column
-        while low <= high:
+        while low < high:
             middle = low + high // 2
             if matrix[middle][0] < target:
                 low = middle + 1
@@ -26,19 +29,21 @@ class Solution:
 
         #binary search the row
         row = high
+        high = len(matrix[row]) - 1
         if row >= 0:
-            for int x = 0; high = len(matrix[row]) - 1; low <= high:
-        
-                    middle = (low + high) / 2;
-                    if (matrix[row][middle] < target):
-                
-                        low = middle + 1;
+            while low <= high:
+                middle = (low + high) // 2;
+                if (matrix[row][middle] < target):
+                    low = middle + 1;
                     
-                    elif (matrix[row][middle] > target):
-                    
-                        high = middle - 1;
-                    
-                    else:
-                        return True;
-            return False;
-            
+                elif (matrix[row][middle] > target): 
+                    high = middle - 1;
+                else:
+                    return True;
+        return False;
+matrix1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+print(len(matrix1))
+obj1 = Solution()
+print(obj1.searchMatrix(matrix1,3))
+   

c58ce6ecae4a07e5fe5fff084cbc2d07c26d4226
LiberoPat <patrick.trias@gmail.com>
2021-12-24 01:39:17 -0800

    completed

---
+++ infSortArray.py
@@ -0,0 +1,50 @@
+"""
+run time OlogN
+Time complex: O1
+
+search array of unknown length
+"""
+
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        
+        
+        end = 1
+        
+        while reader[end] < target:            
+            end *= 2
+            
+            
+        start = 0
+        
+        while start + 1 < end:
+            
+            mid = start + (end - start) // 2
+            
+            if reader[mid] == target:
+                return mid
+            elif reader[mid] < target:
+                start = mid
+            else:
+                end = mid
+                
+        
+        if reader[start] == target:
+            return start
+        
+        if reader[end] == target:
+            return end
+        
+        return -1
+
+secret = [-1,0,3,5,9,12]
+target = 3
+
+obj = Solution()
+
+print(obj.search(secret,target))
---
+++ rotSearchArr.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+       
+        
+        low, high = 0, len(nums)-1
+        
+        while low <= high:
+            mid = (low + high) // 2
+             #base case, target = mid
+            if target == nums[mid]:
+                return mid
+            #check which side is sorted
+            #one side is at least correct, if left is sorted..
+            elif nums[low] <= nums[mid]:
+                #perform bi search, check if target is here
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else: #right side is sorted
+                if target > nums[mid] and nums[high] >= target:
+                    low = mid + 1
+                else:
+                    high = mid - 1;
+        return -1;

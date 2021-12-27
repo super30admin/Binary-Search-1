a8ef754509b12efbca31faa7907c2c4df319d442
LiberoPat <patrick.trias@gmail.com>
2021-12-27 08:01:30 -0800

    updated search matrix with bi-search

--- search2d.py
+++ search2d.py
@@ -6,20 +6,18 @@ refer to middle, compare to target
 
 
 class Solution:
-    def __init__(self):
-        print("in init")
+
 
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-
-
-        #perform 2 binary search, runtime is O(log(mn)
-        if len(matrix[0]) == 0 or len(matrix) == 0:
+    #perform 2 binary search, runtime is O(log(mn)
+        if target is None or not matrix:
             return False
-
-        low = 0; high = len(matrix)
-        #binary search column
-        while low < high:
-            middle = low + high // 2
+        
+        low = 0; high = len(matrix) -1
+        #binary search row[x][0]
+        while low <= high:
+            middle = (low + high) // 2
+            x= matrix[middle][0]
             if matrix[middle][0] < target:
                 low = middle + 1
             elif matrix[middle][0] > target:
@@ -30,9 +28,11 @@ class Solution:
         #binary search the row
         row = high
         high = len(matrix[row]) - 1
+        low = 0;
         if row >= 0:
             while low <= high:
                 middle = (low + high) // 2;
+                y = matrix[row][middle]
                 if (matrix[row][middle] < target):
                     low = middle + 1;
                     
@@ -41,9 +41,14 @@ class Solution:
                 else:
                     return True;
         return False;
+            
+
+
 matrix1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+
+
 
 print(len(matrix1))
 obj1 = Solution()
-print(obj1.searchMatrix(matrix1,3))
+print(obj1.searchMatrix(matrix1,13))
    

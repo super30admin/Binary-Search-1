86879cbcd51a9324db9fc052718dd695839e15d2
alexanderjac <alexander.jac@northeastern.edu>
2022-01-04 00:48:35 -0500

    Enter

---
+++ search element in Sorted array of infinite size.py
@@ -0,0 +1,25 @@
+class InfiniteArray:
+     def __init__(self,reader,target):
+         self.reader = reader
+         self.target = target
+
+     def search(self):
+         low =0
+         high =1
+
+         while(self.reader[high]<self.target):
+             low=high
+             high = 2*high
+
+         while(low<= high):
+             mid = low + (high-low)//2
+             if(self.reader[mid]==self.target): return mid
+             elif(self.reader[mid]>self.target):
+                 high=mid-1
+             else:
+                low = mid+1
+         return -1
+
+
+
+

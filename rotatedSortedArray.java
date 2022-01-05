67485765b28e7e0ffd827e71e84773bee4920372
afrozinamdar-101 <84017292+afrozinamdar101@users.noreply.github.com>
2022-01-05 17:58:31 -0500

    completed

---
+++ rotatedSortedArray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length -1;
+        while(left<=right){
+            int mid = left + ((right-left)/2);
+            
+            if(target == nums[mid]){
+                return mid;
+            }
+            
+            if(nums[mid] >= nums[left]){
+                if(target >= nums[left] && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            
+            else{
+                if(target <= nums[right] && target > nums[mid]){
+                    left = mid + 1;
+                }else{
+                    right = mid -1;
+                }
+            }
+        }
+       return -1; 
+    }
+}

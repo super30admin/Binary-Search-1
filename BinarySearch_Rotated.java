9502732371baed437e6670d4d5cdb7851b5d339b
NachiketBavadekar <nachiket.bavadekar@gmail.com>
2022-03-03 20:52:10 -0600

    Binary Search-1 in Java

---
+++ BinarySearch_Rotated.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(high>=low)
+        {
+            int mid = low+(high-low)/2;
+            
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[low]<=nums[mid])
+            {
+                if(nums[low] <= target && nums[mid] >= target)
+                {
+                    high = mid-1;
+                }
+                else
+                {
+                    low = mid+1;
+                }
+            }
+            else if(nums[mid]<=nums[high])
+            {
+                if(nums[mid] <= target && nums[high] >= target)
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+            }
+        }
+        
+        return -1;
+        
+    }
+}

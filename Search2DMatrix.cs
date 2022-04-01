cf87d561baf2f6832ea44e5a751b999d0196cb5a
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-03-31 22:46:10 -0700

    Add files via upload

---
+++ Search2DMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Search2DMatrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (matrix == null || matrix.Length == 0)
+                return false;
+
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+
+            int start = 0;
+            int end = m * n - 1;
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                int row = mid / n;
+                int column = mid % n;
+                if (matrix[row][column] == target)
+                    return true;
+
+                if (matrix[row][column] < target)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+
+            return false;
+        }
+    }
+}

2b12ef9a3acca58636fc9cc64b20e63e235cfa5d
Rohan <rohantadphale@gmail.com>
2022-04-01 13:42:42 -0400

    Binary-Search-1 Java #Done

---
+++ SearchRotatedSortedArray.java
@@ -0,0 +1,26 @@
+public class SearchRotatedSortedArray {
+    public int search(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0)
+            return -1;
+
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target >= nums[start] && target < nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            } else {
+                if (target <= nums[end] && target > nums[mid])
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

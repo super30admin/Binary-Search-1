4d94ab0c3081989e273eb0344b00c229395d9c0e
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-03-31 23:20:50 -0700

    Add files via upload

---
+++ RotatedSortedArraySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class RotatedSortedArraySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            if(nums==null || nums.Length==0)
+            {
+                return - 1;
+            }
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left<=right)
+            {
+                int mid = left + (right - left)/2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                //left side sorted
+                if(nums[left]<=nums[mid])
+                {
+                    if(nums[left]<=target && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                //right side sorted
+                else
+                {
+                    if (nums[mid] < target && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}

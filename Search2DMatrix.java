a812c2e54a4f5e7bf7b1fe03b184fea4b223bbaa
Rohan <rohantadphale@gmail.com>
2022-04-04 14:58:53 -0400

    Comments updated

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(m+n)
+// Time Complexity : O(m+n) where m is number of columns and n is number of rows
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : trying to decide the limits of the conditions was a bit difficult
--- SearchRotatedSortedArray.java
+++ SearchRotatedSortedArray.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(lon n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
 public class SearchRotatedSortedArray {
     public int search(int[] nums, int target) {
 

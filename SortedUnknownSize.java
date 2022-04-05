b97f9cf8f1bc2dd9e9df858b65a56d446acd82a1
eshasah <esha8sah@gmail.com>
2022-04-05 00:57:20 -0700

    Solution

---
+++ SortedUnknownSize.java
@@ -0,0 +1,35 @@
+// Time Complexity : O(log(n))
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+class SortedUnknownSize {
+    public int search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = 1;
+        
+        while(reader.get(high) < target){
+            low = high;
+            high = 2*high;
+        }
+        return binarySearch(reader, target, low, high);
+    }
+    
+    public int binarySearch(ArrayReader reader, int target, int low, int high){
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(reader.get(mid) == target){
+                return mid;
+            }
+            else if(target < reader.get(mid)){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }            
+        }
+        return -1;
+    }
+}

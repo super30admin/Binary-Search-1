93977250222ef421ac3362695c3dd9b4299120c9
Rincy Soman <rincysoman@gmail.com>
2022-04-29 10:05:38 -0700

    Search in Rotated Array and Unknown Size Array

---
+++ SearchInRotatedArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Algorithms
+{
+    public class SearchInRotatedArray
+    {
+        /// Time Complexity : O(lognß)
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  No
+
+        public int Search(int[] nums, int target)
+        {
+            if (nums != null && nums.Length == 0) return -1;
+            int start = 0; int end = nums.Length - 1;
+            int mid;
+            while (start <= end)
+            {
+                mid = start + (end - start) / 2;
+                if (nums[mid] == target) return mid;
+                //find which side is sorted
+                if (nums[start] <= nums[mid])
+                {
+                    //left side sorted
+                    if (nums[start] <= target && target < nums[mid])
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+                else
+                {
+                    //right side sorted
+                    if (nums[mid] < target && target <= nums[end])
+                    {
+                        start = mid + 1;
+                    }
+                    else
+                    {
+                        end = mid - 1;
+                    }
+
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ SearchInUnknownSizeArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Algorithms
+{
+    public class SearchInUnknownSizeArray
+    {
+        /// Time Complexity : O(log n)
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  No
+        public int Search(ArrayReader reader, int target)
+        {
+            int low = 0;
+            int high = 1;
+
+            while (reader.get(high) < target)
+            {
+                low = high;
+                high = high * 2;
+            }
+
+
+            int mid;
+            //do BST in the boundary
+            while (low <= high)
+            {
+                mid = low + (high - low) / 2;
+                if (reader.get(mid) == target) return mid;
+                if (reader.get(mid) > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+
+        }
+    }
+}

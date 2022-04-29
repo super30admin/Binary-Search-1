8c8c7f4b491cb7167086eca18b61cf5cb91edc2b
Mrigesh Dasgupta <dasgupta.m@northeastern.edu>
2022-04-28 21:12:32 -0400

    Optimized Soultion 1

--- Search2DMatrix.java
+++ Search2DMatrix.java
@@ -1,13 +1,26 @@
+//TC = O(log(row*col))
+//Sc  =O(1)
+
 class Solution {
-	
-// Brute Force Approach
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i =0; i<matrix.length;i=i+1){
-            for(int j=0;j<matrix[i].length;j=j+1){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+       
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        int low=0;
+        int high = (row*col)-1;
+        // mid/col => Row index in Matrix
+        // mid%col => Column index in matrix
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(matrix[mid/col][mid%col]==target){
+                return true;
+            }else if(matrix[mid/col][mid%col] < target){
+               low=mid+1;;
+            }else if(matrix[mid/col][mid%col] > target){
+                high = mid-1;
             }
+            
         }
         return false;
     }

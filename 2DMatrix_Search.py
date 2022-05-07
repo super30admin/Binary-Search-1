a7b3dd5881f08e8061e6fb2c7c48ba35de32ff44
amby2964 <75859704+amby2964@users.noreply.github.com>
2022-05-07 13:03:21 -0700

    Update 2DMatrix_Search.py

--- 2DMatrix_Search.py
+++ 2DMatrix_Search.py
@@ -1,6 +1,27 @@
 # 2 Pointer Solution: Searching in a 2D matrix
 # Complexity: O(m+n)
 class Solution:
+    def searchMatrix1(self, matrix: List[List[int]], target: int) -> bool:
+        # binary search solution, O(log(mn))
+        m = len(matrix)
+        n = len(matrix[0])
+        beg = 0
+        end = m*n-1
+        
+        while(beg<=end):
+            mid = (beg + end)//2
+            r = mid//n
+            c = mid%n
+            if(matrix[r][c]==target):
+                return True
+            
+            if matrix[r][c]>target:
+                end = mid - 1
+            else:
+                beg = mid + 1
+        
+        return False
+    
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         if(len(matrix)==0 or len(matrix[0])==0):
             return False

b3b7a00cd1967268a7139f73df53f2468711d8e2
Gaurav Tyagi <gauravtyagi777@gmail.com>
2022-05-27 17:38:42 -0500

    Create Search_in_rotated_sorted_Array.py

---
+++ Search_in_rotated_sorted_Array.py
@@ -0,0 +1,25 @@
+'''Time complexity : O(log(n)
+Space COmplexity : O(1)
+'''
+
+class Solution(object):
+    def search(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        
+        while (l<=r) :
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if target < nums[mid] and target >= nums[l]:
+                    r = mid -1
+                else:
+                    l = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                
+        return -1

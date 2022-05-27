35f8d9b810f7ae27660b9155aff3e98217779eb3
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-05-27 11:13:52 -0400

    Update Solution.java

--- Search in a Rotated Sorted Array/Solution.java
+++ Search in a Rotated Sorted Array/Solution.java
@@ -2,28 +2,29 @@
 // space complexity = O(n)
 class Solution {
     public int search(int[] nums, int target) {
-        int minIndex=0;
-        int x;
-        for(int i=0;i<nums.length;i++){
-            if(nums[minIndex]>nums[i]) minIndex=i;
+        int low = 0;
+        int high = nums.length-1;
+        if(nums[low]==target) return low;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[low]<=nums[mid]){ //left sorted ; add '='
+                if(nums[low]<=target && target<nums[mid]){ //add '='
+                    high=mid; //high=mid-1;
+                } else {
+                    low=mid+1;
+                }
+            } else{
+                //right sorted
+                if(target>nums[mid]&&target<=nums[high]){ //add '='
+                    low=mid+1;
+                } else{
+                    high=mid;//   high=mid-1;
+                }
+            }
+
         }
-
-        if(nums[minIndex]==target) return minIndex;
-        x = binarySearch(nums,0,minIndex-1,target);
-        if(x!=-1) return x;
-        x = binarySearch(nums,minIndex+1,nums.length-1,target);
-        if(x!=-1) return x;
+        if(nums[low]==target) return low;
         return -1;
-    }
-
-    public int binarySearch(int nums[], int l, int h, int target){
-        int x=-1;
-        if(l>h) return -1;
-        int m = (l+h)/2;
-        System.out.println("mid: "+m);
-        if(nums[m]==target) return m;
-        else if(nums[m]<target) x= binarySearch(nums,m+1,h,target);
-        else x= binarySearch(nums,l,m-1,target);
-        return x;
     }
 }

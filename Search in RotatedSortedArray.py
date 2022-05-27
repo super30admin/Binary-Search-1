e3a15e24715a6ef9fb8d27e0abcd30f3d384a417
rohithjallipalli <rohith.jallipalli@gmail.com>
2022-05-27 14:08:28 -0500

    Done

--- Search in RotatedSortedArray.py
+++ Search in RotatedSortedArray.py
@@ -1,10 +1,8 @@
-""""// Time Complexity : O(1)
+""""// Time Complexity : O(n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :
-
-
-// Your code here along with comments explaining your approach"""
+"""
 
 
 class Solution:
--- Search in SortedArray of Unknown length.py
+++ Search in SortedArray of Unknown length.py
@@ -1,16 +1,19 @@
-""""// Time Complexity : O(1)
+""""// Time Complexity : O(log(n))
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :
+"""
 
 
-// Your code here along with comments explaining your approach"""
-
-
-
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
 
 class Solution:
-    def search(self, nums, target):
+    def search(self, reader: 'ArrayReader', target: int) -> int:
 
 
         # O(log n)
@@ -18,7 +21,7 @@ class Solution:
         right = 1
 
         # firstly we decide our boundaries for search
-        while nums[right] < target:
+        while reader.get(right) < target:
             left = right
             right *=2
 
@@ -26,14 +29,14 @@ class Solution:
         # now we run our normal binary search
 
         while left<=right :
-            mid = left=(right-left)//2
+            mid = (left+right)//2
 
-            if target == nums[mid]:
+            if target == reader.get(mid):
                 return mid
 
-            if target<nums[mid]:
+            if target<reader.get(mid):
                 right = mid-1
-            elif target > nums[mid]:
+            elif target > reader.get(mid):
                 left = mid+1
 
         return -1

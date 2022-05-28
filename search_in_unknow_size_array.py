b4e2f00bde1b7b52cf2d65d3ef47c475b54c4af2
Sandeep Kumar Reddy Onteddy (Student) <sonteddy@sundevils.asu.edu>
2022-05-27 18:01:48 -0700

    Completed Binary-Search-1 Please review

---
+++ search_in_unknow_size_array.py
@@ -0,0 +1,25 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+# Time Complexity: O(log(n))
+# Space Complexity: O(1)
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        low=0
+        high=1
+        while reader.get(high)< target:
+            low=high
+            high=high*2
+        while low<=high:
+            mid=low+ (high-low)//2
+            if reader.get(mid)==target:
+                return mid
+            elif reader.get(mid)<target:
+                low= mid+1
+            else:
+                high=mid-1
+        return -1

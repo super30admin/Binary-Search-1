17bc91eea510ffafc9a59a4a45625334dd6bf5f2
NishanthSayana <snvnishanth99@gmail.com>
2022-05-29 11:06:44 +0530

    Binary Search-1 Please review

---
+++ SearchInSortedUnkownSize.java
@@ -0,0 +1,21 @@
+public class SearchInSortedUnkownSize {
+
+    public int search(ArrayReader reader, int target) {
+
+        int low=0,high=1;
+
+        while(reader.get(high)<target){
+            low=high;
+            high=2*high;
+        }
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(reader.get(mid)==target) return mid;
+            else if(reader.get(mid)>target) high=mid-1;
+            else low=mid+1;
+        }
+
+        return -1;
+    }
+}

b216fcf1b46fb7cfeba97603b41a04285d6f14a5
Chirag Dara <chiragdara23@gmail.com>
2022-06-23 11:20:07 -0500

    Leetcode : 74 - Search a 2D Matrix

--- Search_2D_Matrix.java
+++ Search_2D_Matrix.java
@@ -1,5 +1,5 @@
 /*
-* Time Complexity : O(NlogN)
+* Time Complexity : O(M)+O(logN)
 * Space Complexity: O (1)
 * */
 public class Search_2D_Matrix {
@@ -17,6 +17,38 @@ public class Search_2D_Matrix {
                         high = mid - 1;
                     }
                 }
+            }
+        }
+        return false;
+    }
+}//-----------------------------------------------------------------------
+
+/* Optimized Code
+*  Time Complexity:  O(log m + n )
+*  Space Complexity: O(1)
+*  */
+
+public class Search_2D_Matrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // Checking base condition
+        if(matrix == null || matrix.length == 0) return false;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+
+        while(low <= high){
+            int middle = low + (high-low)/2;
+            int row = middle / n;
+            int col = middle % n;
+
+            if(matrix[row][col] == target){
+                return true;
+            } else if(target < matrix[row][col]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
             }
         }
         return false;

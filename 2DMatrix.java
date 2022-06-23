7ce9a4b65b4373090b7e6ec59ddc25210ac4034c
Pavan Reddy Gottimukkula <46338498+pavanreddy21@users.noreply.github.com>
2022-06-23 14:52:06 +0000

    Problem 1

---
+++ 2DMatrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length==0){
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int start = 0;
+        int end = (rows * cols) - 1;
+        int mid;
+        while(start<=end){
+            mid = start + (end - start)/2;
+            int row = mid / cols;
+            int col = mid % cols;
+            if(matrix[row][col] == target){
+                return true;
+            }
+            if(matrix[row][col] > target){
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}

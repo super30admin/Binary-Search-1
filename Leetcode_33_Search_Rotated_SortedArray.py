a2baa3553652cf96a3da4b001f2850fb6311d365
Rutvik Avaiya <rutvikavaiya01@gmail.com>
2022-06-24 07:14:17 -0500

    Sprint Completed

---
+++ Leetcode_33_Search_Rotated_SortedArray.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(logn)
+# Space Complexity: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            if nums[left]<=nums[mid]:
+                if target>=nums[left] and target< nums[mid]:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else:
+                if target<=nums[right] and target>nums[mid]:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1

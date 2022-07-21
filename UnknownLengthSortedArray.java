14a73be3ca73b136d55ae66c1f79bb14f315cbd1
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-21 00:14:31 -0400

    Update UnknownLengthSortedArray.java

--- UnknownLengthSortedArray.java
+++ UnknownLengthSortedArray.java
@@ -1,3 +1,11 @@
+// Time Complexity : O(long(n)) since we are reducing the search by half array each iteration.
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// Following normal 2 pointer algorithm for binary search tree but since we dont have length of array, I am taking int max value.
+
 class Solution {
     public int search(ArrayReader reader, int target) {
         int startIndex = 0,endIndex = Integer.MAX_VALUE;

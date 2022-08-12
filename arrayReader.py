1b66148f9cf7112d229907926db7adeff853691c
Vignesh Viswanathan <vigneshviswanathan@Vigneshs-MacBook-Pro.local>
2022-08-11 21:21:26 -0400

    Completed Binary Search - 1 All Problems

--- arrayReader.py
+++ arrayReader.py
@@ -5,6 +5,14 @@
 # class ArrayReader:
 #    def get(self, index: int) -> int:
 
+# Time Complexity: O(logn): This is because each time half of the search space is reduced. We are using Binary Search for it
+# Space Complexity: O(1)
+
+# This approach uses the Binary search. We keep decreasing our search space first to find the High Pointer.
+
+# Number 2 is used because of the reason that, if we keep increasing 3, 4, 5 ... Eventually it is going to n, because, 1/3 is better than 1/2, 1/4 better than 1/2 .... 1/n better than 1/n-1
+# Once we get the range where the target lies, then it is a simple Binary search
+
 
 class Solution:
     def search(self, reader: "ArrayReader", target: int) -> int:

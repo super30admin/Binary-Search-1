893c06f7937be7adec2f12dece616623b6d9bc29
Gowtham Chandrasekaran <gowtham6309@gmail.com>
2022-08-18 15:52:16 -0700

    problem-3 solution updated

--- problem-3.py
+++ problem-3.py
@@ -1,3 +1,8 @@
+# Time Complexity : O(log(n))
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
 # """
 # This is ArrayReader's API interface.
 # You should not implement it, or speculate about its implementation

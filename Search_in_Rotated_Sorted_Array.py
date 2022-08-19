bd9f8d0f28819a60bfa62bbfc65d4369cd7d00a4
Rajendra Prasad Patil <111140820+rajendra7406-2@users.noreply.github.com>
2022-08-19 12:25:17 -0400

    Update Search_in_Rotated_Sorted_Array.py

--- Search_in_Rotated_Sorted_Array.py
+++ Search_in_Rotated_Sorted_Array.py
@@ -1,36 +1,32 @@
+# binary search in rotating array
+# TIME COMPLEXITY: O(logN)
+# SPACE COMPLEXITY: O(1)
+# Yes, it passed all test cases in the leetcode
+# Any problems faced? I was able to code after I learned how to solve it.
+# Unfortunately, couldn't figure out how to solve first time.
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         i = 0
         j = len(nums)-1
         
-        # find the pivot (smallest number)
-        # k is the index
+        # kind of tricky, and it is easy after you understand 
+        
         while i <= j:
             k = (i+j)//2
-            if nums[k] == target:
+            if target == nums[k]:
                 return k
-            elif nums[k] < nums[j]:
-                j = k
-            elif nums[k] > nums[j]:
-                i = k
+            elif nums[i] <= nums[k]:
+                # array is left sorted
+                # then figure out if target lies b/w i and k
+                if nums[i] <= target and target < nums[k]:
+                    j = k -1
+                else:
+                    i = k+1
             else:
-                j = k
-        
-        i = 0
-        j = len(nums)-1
-        
-        if target <= nums[j] and target >= nums[k]:
-            i = k
-        else:
-            j = k-1
-            
-        while i <= j:
-            k = (i+j)//2
-            if nums[k] == target:
-                return k
-            elif target >= nums[k]:
-                i = k+1
-            else:
-                j = k-1
-        
+                # array is right sorted
+                if nums[j] >= target and target > nums[k]:
+                    i =k+1
+                else:
+                    j = k-1
         return -1

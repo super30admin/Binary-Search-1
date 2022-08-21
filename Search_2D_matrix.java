dd2e4a874377d61efe62f14722ee1640fb724cc0
Sujan Rao Chikkela <sujanraochikkela@gmail.com>
2022-08-20 23:23:41 -0700

    Update Search_2D_matrix.java

--- Search_2D_matrix.java
+++ Search_2D_matrix.java
@@ -1,3 +1,8 @@
+//time commplexity - O(logn)
+
+//space complexity - O(1)
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix==null || matrix.length==0) return false;

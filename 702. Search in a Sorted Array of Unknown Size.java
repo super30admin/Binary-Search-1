89bf709fdf03d583dead65e876862af836bed650
cskp009 <110018417+cskp009@users.noreply.github.com>
2022-08-22 14:01:09 -0500

    Create 702. Search in a Sorted Array of Unknown Size.java

---
+++ 702. Search in a Sorted Array of Unknown Size.java
@@ -0,0 +1,13 @@
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        
+    }
+}

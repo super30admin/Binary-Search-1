961ef3f855f3888784b6c6778917128a4eed3ea3
Mohit Nagpal <mohit@Mohits-MBP.attlocal.net>
2022-09-14 15:38:11 -0400

    Completed Binary Search 1

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+// Time Complexity :O(logn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return -1;
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}

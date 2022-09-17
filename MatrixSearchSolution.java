dd09740bfb0d5859049da753aac40b445a57e295
Sumanth Shankar <sumanth@rezy360.com>
2022-09-17 16:10:30 -0700

    Matrix search solution

---
+++ MatrixSearchSolution.java
@@ -0,0 +1,29 @@
+class MatrixSearchSolution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) {
+            return false;
+        }
+        //get rows and cols length
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        //initilize low and high for binary search
+        int low = 0;
+        int high = (rows * cols) - 1;
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            //Calculating row and col using mid
+            int row = mid / cols;
+            int col = mid % cols;
+            if(matrix[row][col] == target) {
+                return true;
+            } else if(target > matrix[row][col] ) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}

b4ec8f09c358e868beab1978b8b10e6dbabaec21
Tejasvib <tejasvibhadrinath26@gmail.com>
2022-09-17 12:26:42 -0400

    made changes according to the reviw

--- Search2D.java
+++ Search2D.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(logn)
+// Time Complexity : O(log mn)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No

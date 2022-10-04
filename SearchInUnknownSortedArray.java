0b5891ec1a7e41465202cd5cfc9b3520a01eed42
anrg-ydv <110518640+anrg-ydv@users.noreply.github.com>
2022-10-04 12:43:11 +0530

    Update SearchInUnknownSortedArray.java
    Implemented successfully

--- SearchInUnknownSortedArray.java
+++ SearchInUnknownSortedArray.java
@@ -34,3 +34,30 @@ class Solution {
         return -1;
     }
 }
+
+
+/**
+ * Another approach
+ * Since elements are unique. We can directly find the high as target - reader.get(low)
+ * this will reduce our TC by O(logM) which was used in previous solution for incrementing High by 2
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {        
+        int low = 0; 
+        int high = target - reader.get(low);
+                
+        while (low <= high){        // BS => TC = O(logN)
+            int mid = low + (high - low)/2;
+            int midVal = reader.get(mid);
+            if (midVal == target) return mid;
+            else if (midVal < target){
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+}
+// TC = O(logN)

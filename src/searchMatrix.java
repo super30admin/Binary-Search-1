1340f7e7808b29a5f2e28871f3f2ab394f6eb25b
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-10-05 16:48:50 -0400

    search in a matrix

---
+++ src/searchMatrix.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class searchMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target){
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int low = 0;
+        int high = m*n-1;
+
+        int index;
+        int element;
+
+        while(low <= high){
+
+            index = (low+high)/2;
+            element = matrix[index/n][index%n];
+
+            if( target == element){
+                return true;
+            }else{
+
+                if( target < element){
+                    high = index-1;
+                }else{
+                    low = index+1;
+                }
+
+            }
+
+        }
+
+        return false;
+    }
+
+}

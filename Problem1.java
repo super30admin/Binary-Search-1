2132f3145f4029a708036a6bbfaed1de877ebc2b
Siddhant Satish Parmar <siddhantsatish.parmar@sjsu.edu>
2022-10-07 16:35:49 -0700

    Completed Problem 2

--- Problem1.java
+++ Problem1.java
@@ -1,4 +1,4 @@
-// Time Complexity :
+// Time Complexity : O(m.logn)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No

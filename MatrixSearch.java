83e6ec9097a86722a66e4847663308e2592f5a40
katta332 <praveenkumar.kpc@gmail.com>
2022-10-26 21:34:23 -0600

    Completed Binary search 1, please review

--- MatrixSearch.java
+++ MatrixSearch.java
@@ -1,57 +1,43 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;\f1\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red12\green99\blue153;\red0\green0\blue0;\red210\green50\blue85;
-\red135\green135\blue135;\red135\green91\blue44;\red255\green255\blue255;\red133\green0\blue67;}
-{\*\expandedcolortbl;;\cssrgb\c0\c46667\c66667;\cssrgb\c0\c0\c0;\cssrgb\c86667\c29020\c40784;
-\cssrgb\c60000\c60000\c60000;\cssrgb\c60392\c43137\c22745;\cssrgb\c100000\c100000\c100000\c50196;\cssrgb\c60000\c0\c33333;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 //time complexity is O(N)\
-//space complexity is O(1)\
-//able to submit the code in leetcode\
-class\cf0 \strokec3  \cf4 \strokec4 Solution\cf0 \strokec3  \cf5 \strokec5 \{\cf0 \strokec3 \
-    \cf2 \strokec2 public\cf0 \strokec3  \cf2 \strokec2 boolean\cf0 \strokec3  \cf4 \strokec4 searchMatrix\cf5 \strokec5 (\cf2 \strokec2 int\cf5 \strokec5 [][]\cf0 \strokec3  matrix\cf5 \strokec5 ,\cf0 \strokec3  \cf2 \strokec2 int\cf0 \strokec3  target\cf5 \strokec5 )\cf0 \strokec3  \cf5 \strokec5 \{\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  m \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  matrix\cf5 \strokec5 .\cf0 \strokec3 length\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  n \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  matrix\cf5 \strokec5 [\cf8 \strokec8 0\cf5 \strokec5 ].\cf0 \strokec3 length\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  l \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  \cf8 \strokec8 0\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  r \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  \cf5 \strokec5 (\cf0 \strokec3 m\cf6 \cb7 \strokec6 *\cf0 \cb1 \strokec3 n\cf5 \strokec5 )\cf6 \cb7 \strokec6 -\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 while\cf5 \strokec5 (\cf0 \strokec3 l\cf6 \cb7 \strokec6 <=\cf0 \cb1 \strokec3 r\cf5 \strokec5 )\{\cf0 \strokec3 \
-            \cf2 \strokec2 int\cf0 \strokec3  pivotidx \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  \cf5 \strokec5 (\cf0 \strokec3 r\cf6 \cb7 \strokec6 +\cf0 \cb1 \strokec3 l\cf5 \strokec5 )\cf6 \cb7 \strokec6 /\cf8 \cb1 \strokec8 2\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf2 \strokec2 int\cf0 \strokec3  pivotelm \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  matrix\cf5 \strokec5 [\cf0 \strokec3 pivotidx\cf6 \cb7 \strokec6 /\cf0 \cb1 \strokec3 n\cf5 \strokec5 ][\cf0 \strokec3 pivotidx\cf6 \cb7 \strokec6 %\cf0 \cb1 \strokec3 n\cf5 \strokec5 ];\cf0 \strokec3 \
-            \cf2 \strokec2 if\cf5 \strokec5 (\cf0 \strokec3 pivotelm \cf6 \cb7 \strokec6 ==\cf0 \cb1 \strokec3  target\cf5 \strokec5 )\{\cf0 \strokec3 \
-                \cf2 \strokec2 return\cf0 \strokec3  \cf8 \strokec8 true\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-            \cf2 \strokec2 else\cf0 \strokec3  \cf2 \strokec2 if\cf5 \strokec5 (\cf0 \strokec3 pivotelm\cf6 \cb7 \strokec6 <\cf0 \cb1 \strokec3 target\cf5 \strokec5 )\{\cf0 \strokec3 \
-                l \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3 pivotidx\cf6 \cb7 \strokec6 +\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-            \cf2 \strokec2 else\cf5 \strokec5 \{\cf0 \strokec3 \
-                r \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  pivotidx\cf6 \cb7 \strokec6 -\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-        \cf5 \strokec5 \}\cf0 \strokec3 \
-        \cf2 \strokec2 return\cf0 \strokec3  \cf8 \strokec8 false\cf5 \strokec5 ;\cf0 \strokec3 \
-    \cf5 \strokec5 \}\cf0 \strokec3 \
-    \cf2 \strokec2 public\cf0 \strokec3  \cf2 \strokec2 boolean\cf0 \strokec3  \cf4 \strokec4 helper\cf5 \strokec5 (\cf2 \strokec2 int\cf5 \strokec5 []\cf0 \strokec3  arr\cf5 \strokec5 ,\cf0 \strokec3  \cf2 \strokec2 int\cf0 \strokec3  target\cf5 \strokec5 )\{\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  l\cf6 \cb7 \strokec6 =\cf8 \cb1 \strokec8 0\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 int\cf0 \strokec3  r \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  arr\cf5 \strokec5 .\cf0 \strokec3 length\cf6 \cb7 \strokec6 -\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-        \cf2 \strokec2 while\cf5 \strokec5 (\cf0 \strokec3 l\cf6 \cb7 \strokec6 <=\cf0 \cb1 \strokec3 r\cf5 \strokec5 )\{\cf0 \strokec3 \
-            \cf2 \strokec2 int\cf0 \strokec3  m \cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3  l\cf6 \cb7 \strokec6 +\cf5 \cb1 \strokec5 (\cf0 \strokec3 r\cf6 \cb7 \strokec6 -\cf0 \cb1 \strokec3 l\cf5 \strokec5 )\cf6 \cb7 \strokec6 /\cf8 \cb1 \strokec8 2\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf2 \strokec2 if\cf5 \strokec5 (\cf0 \strokec3 arr\cf5 \strokec5 [\cf0 \strokec3 m\cf5 \strokec5 ]\cf6 \cb7 \strokec6 ==\cf0 \cb1 \strokec3 target\cf5 \strokec5 )\{\cf0 \strokec3 \
-                \cf2 \strokec2 return\cf0 \strokec3  \cf8 \strokec8 true\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-            \cf2 \strokec2 else\cf0 \strokec3  \cf2 \strokec2 if\cf5 \strokec5 (\cf0 \strokec3 arr\cf5 \strokec5 [\cf0 \strokec3 m\cf5 \strokec5 ]\cf6 \cb7 \strokec6 <\cf0 \cb1 \strokec3 target\cf5 \strokec5 )\{\cf0 \strokec3 \
-                l\cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3 m\cf6 \cb7 \strokec6 +\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-            \cf2 \strokec2 else\cf0 \strokec3 \
-            \cf5 \strokec5 \{\cf0 \strokec3 \
-                r\cf6 \cb7 \strokec6 =\cf0 \cb1 \strokec3 m\cf6 \cb7 \strokec6 -\cf8 \cb1 \strokec8 1\cf5 \strokec5 ;\cf0 \strokec3 \
-            \cf5 \strokec5 \}\cf0 \strokec3 \
-        \cf5 \strokec5 \}\cf0 \strokec3 \
-        \cf2 \strokec2 return\cf0 \strokec3  \cf8 \strokec8 false\cf5 \strokec5 ;\cf0 \strokec3 \
-    \cf5 \strokec5 \}\cf0 \strokec3 \
-\pard\pardeftab720\partightenfactor0
-\cf5 \strokec5 \}
-\f1 \cf0 \strokec3 \
+//time complexity is O(logN)
+//space complexity is O(1)
+//able to submit the code in leetcode
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l = 0;
+        int r = (m*n)-1;
+        while(l<=r){
+            int pivotidx = (r+l)/2;
+            int pivotelm = matrix[pivotidx/n][pivotidx%n];
+            if(pivotelm == target){
+                return true;
+            }
+            else if(pivotelm<target){
+                l =pivotidx+1;
+            }
+            else{
+                r = pivotidx-1;
+            }
+        }
+        return false;
+    }
+    public boolean helper(int[] arr, int target){
+        int l=0;
+        int r = arr.length-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(arr[m]==target){
+                return true;
+            }
+            else if(arr[m]<target){
+                l=m+1;
+            }
+            else
+            {
+                r=m-1;
+            }
+        }
+        return false;
+    }
 }

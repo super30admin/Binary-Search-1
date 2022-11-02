9da3d3690728899b87fc9ad7cb9d11835f986ac4
ZeelSatasiya <zeel.satasiya@gmail.com>
2022-11-01 21:35:16 -0700

    Binary Search 1 Completed

---
+++ Problem_3.java
@@ -0,0 +1,8 @@
+// Time Complexity :O(log(n))
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes
+
+
+// Your code here along with comments explaining your approach
+// Search in Infinite sorted array

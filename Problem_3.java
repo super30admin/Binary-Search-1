8e0a305129fcad8ac8bfaeada719a0d175d4ff25
ZeelSatasiya <zeel.satasiya@gmail.com>
2022-11-01 21:42:29 -0700

    Binary Search 1 Completed all

--- Problem_3.java
+++ Problem_3.java
@@ -6,3 +6,38 @@
 
 // Your code here along with comments explaining your approach
 // Search in Infinite sorted array
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface ArrayReader {
+ *     public int get(int index) {}
+ * }
+ */
+
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = 1;
+        int mid;
+        if(reader.get(0) == target){
+            return 0;
+        }
+        while (reader.get(high) < target){
+            low = high;
+            high = high*2;
+        }
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if (target == reader.get(mid)){
+                return mid;
+            }
+            if(reader.get(mid) < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

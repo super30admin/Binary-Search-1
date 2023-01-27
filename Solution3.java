abb544d102b3d6d2610aa4aba7430bc30ef3e9af
anirudhv98 <vanirudh98@gmail.com>
2023-01-27 13:56:41 -0700

    Completed Search in rotated sorted array

---
+++ Solution3.java
@@ -0,0 +1,36 @@
+class Solution3 {
+    public int search(int[] nums, int target) {
+        int low = 0,high=nums.length-1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[low]<=nums[mid])
+            {
+                if(nums[low]<=target && nums[mid]>target)
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(nums[mid]<target && target<=nums[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}

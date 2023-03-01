1cb4c5a5079d8bfddbd278317c6d5b108d048986
virat02 <virat.goradia96@gmail.com>
2023-02-28 19:40:23 -0800

    Implement search in a 2d array

---
+++ search_a_2D_array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix[0] == null) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // If target is smaller than smallest or larger than largest, don't search at all
+        if (target > matrix[m-1][n-1] || target < matrix[0][0]) {
+            return false;
+        }
+
+        int row = 0;
+        int col = n - 1;
+
+        // While within bounds
+        while (row < m && col >= 0) {
+            // Target found
+            if (target == matrix[row][col]) {
+                return true;
+            } 
+            // Move left
+            else if (target < matrix[row][col]) {
+                col -= 1;
+            } 
+            // Move down
+            else {
+                row += 1;
+            }
+        }
+
+        return false;
+    }
+}

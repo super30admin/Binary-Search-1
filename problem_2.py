b805e187c7b089b74b7abdb6dca46fbfa4605163
Alekhya1311 <alekhyabairaboina1311@gmail.com>
2023-03-02 13:26:22 -0800

    completed search in a sorted array of unknown length

--- problem_2.py
+++ problem_2.py
@@ -1,32 +1,27 @@
-# Time Complexity : O(logn)
-# Space Complexity : O(1)
-# Did this code successfully run on Leetcode : Yes
-
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)-1
-       
+    def search(self, reader: 'ArrayReader', target: int) -> int:
 
-        while left <= right: 
-            mid = (left+right)// 2 
+        low = 0
+        high = 1
 
-
-            if nums[mid] == target: 
-                return mid 
-            elif nums[mid] >= nums[left]:  #left sub array is sorted
-                if target < nums[mid] and target >= nums[left]: 
-                    right = mid -1 
-                else : 
-                    left = mid+1 
-            else : # right sub array is sorted
-                if target > nums[mid] and target <= nums[right]: 
-                    left = mid + 1 
-                else : 
-                    right = mid -1 
-
-        return -1 
-
-
+        while reader.get(high) < target : 
+            low = high
+            high = 2 * high 
         
+        while low <= high: 
+            mid = low + (high-low) // 2 
+            if reader.get(mid) == target: 
+                return mid 
+            elif  reader.get(mid) > target: 
+                high = mid -1 
+            else : 
+                low = mid + 1
+                
+        return -1 

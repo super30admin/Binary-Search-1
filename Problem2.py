5d3fa18d775a750a6511170c816df6df24f9d977
anirudhrani <anirudhrvs@gmail.com>
2023-03-02 20:41:34 -0500

    Implemented the solution discussed in class.

--- Problem2.py
+++ Problem2.py
@@ -13,16 +13,15 @@ class Solution(object):
             if target== nums[mid]:
                 return mid
             
-            # LSA
-            if nums[mid] >= nums[l]:
-                if target> nums[mid] or target< nums[l]:
-                    l= mid+1
+            # Checking if LSA is sorted.
+            if nums[l]<= nums[mid]:
+                if target >=nums[l] and target< nums[mid]:
+                    r= mid-1 # Target is in the left sorted portion
                 else:
-                    r= mid-1
-            # RSA
+                    l= mid+1 # Target is on the right side.
             else:
-                if nums[mid] > target or target> nums[r]:
-                    r= mid-1
-                else:
+                if target <=nums[r] and target >nums[mid]:
                     l= mid +1
+                else:
+                    r= mid -1
         return -1

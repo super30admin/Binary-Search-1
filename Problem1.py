1c5522dac05452659c9d3aa12f7c69d180bc9457
AnirudhS30 <127216139+AnirudhS30@users.noreply.github.com>
2023-03-16 20:37:09 -0400

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,28 +1,26 @@
-class Solution(object):
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if matrix== None and len(matrix)==0:
+            return False
 
-    def binary_search(self, row, low, high, target):
-        if high>=low:
-            mid= low+ (high-low)//2
-            if row[mid] == target:
+        # n-> Rows
+        n= len(matrix) #3
+        #m-> Columns
+        m= len(matrix[0])# 4
+        low= 0
+        high= m * n - 1
+        
+        while low<=high:
+            # Assume it as a 1D array and compute the mid.
+            mid= low +(high-low)//2 # 5 
+            row= mid//m #5//4 ->1
+            column= mid % m # 5%4 ->1
+            if target== matrix[row][column]:
                 return True
-            
-            elif row[mid]> target:
-                return self.binary_search(row, low, mid-1, target)
+            elif target < matrix[row][column]:
+                high= mid-1
             else:
-                return self.binary_search(row, mid+1, high, target)
-            
-        return False
-
-
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        for i in matrix:
-            if self.binary_search(i, 0, len(i)-1,target):
-                return True
+                low= mid+1
         return False
 
 arr= [[1,3]]

b7089864b0cc20e979f85a5066795df44a5119ba
unknown <singhshauryaditya@gmail.com>
2023-04-14 17:55:01 +0530

    Completed search in 2D Matrix

---
+++ 2DMatrixSearch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        while (low <= high) {
+            int mid = high - (high - low)/2;
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            if (matrix[row][col] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;                
+
+            }
+        }
+        return false;
+    }
+}

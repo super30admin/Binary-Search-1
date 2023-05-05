0899c584f20984f908f7db3bce0f070dda3f702b
lohitb009 <45014992+lohitb009@users.noreply.github.com>
2023-05-04 23:15:07 -0400

    c# submission
    c# submission

---
+++ SearchInARotatedSortedArray.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Time Complexity: 0(log(m))
+ * Space Complexity: 0(1)
+ */
+namespace LeetCodeSubmission.BinarySearch1;
+
+public class SearchInARotatedSortedArray
+{
+    public int Search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (high - low) / 2 + low;
+            
+            if (nums[mid] == target)
+            {
+                // return result case
+                return mid;
+            }
+            else
+            {
+                if (nums[mid] > target)
+                {
+                    if (nums[low] <= nums[mid] && nums[low] > target)
+                    {
+                        // sorted lhs; search in unsorted part
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+                else if (nums[mid] < target)
+                {
+                    if (nums[mid] <= nums[high] && nums[high] < target)
+                    {
+                        // sorted rhs; search in unsorted part
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ SearchInASortedArrayOfUnknownSize.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Time Complexity: 0(log(m))
+ * Space Complexity: 0(1)
+ */
+namespace LeetCodeSubmission.BinarySearch1;
+
+/**
+ * // This is ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *     public int Get(int index) {}
+ * }
+ */
+
+public class SearchInASortedArrayOfUnknownSize
+{
+    public int Search(ArrayReader reader, int target) {
+        
+        int low = 0;
+        int high = 1;
+        
+        int idx = -1;
+        
+        while(low <= high){
+            int mid = (high-low)/2 + low;
+            
+            if(reader.get(mid) == target){
+                idx = mid;
+                break;
+            }
+            
+            else if(reader.get(mid) < target){
+                
+                low = mid+1;
+                
+                if(low == high){
+                    high = 2*high;
+                }
+            }
+            
+            else if(reader.get(mid) > target){
+                high = mid-1;
+            }
+            
+        }
+        
+        return idx;
+    }
+}

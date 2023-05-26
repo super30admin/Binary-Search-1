c2cf6b2fb86c2273b2e7925ae08f2f88f3cc678a
KISHORESHANKAR20062000 <60849170+KISHORESHANKAR20062000@users.noreply.github.com>
2023-05-26 14:37:06 -0700

    Completed Matrix and 2dArray exercise

--- matrix2d.py
+++ matrix2d.py
@@ -1,6 +1,13 @@
+#time complexity: O(log m*n)
+#Did the code run in leetcode: Yess the code passed all the test cases.
+#Did you face any problem: No
+
+
 class Solution:
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        #This method is very similart to binary search but this binary search happens in 2d array.
         for i in range(len(matrix)):
             l=0
             h=len(matrix[i])-1
@@ -16,8 +23,3 @@ class Solution:
                 
                     
 
-matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-target = 5
-
-obj = Solution()
-obj.searchMatrix(matrix, target)

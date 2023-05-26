984f1a3982f30d87e089a4af2e1e767942acc7bb
vyankateshmisar <venkymisar@gmail.com>
2023-05-26 09:17:26 -0400

    New Approach for Problem_6

--- Problem_6.py
+++ Problem_6.py
@@ -40,3 +40,24 @@ class Solution(object):
             else:
                 return True
         return False
+
+# Approach 2
+
+# class Solution(object):
+#     def searchMatrix(self, matrix, target):
+#         m=len(matrix)
+#         n=len(matrix[0])
+#         l,h=0,m*n-1
+
+#         while l<=h:
+#             mid=l-(l-h)/2
+#             r=mid/n
+#             c=mid%n
+#             if matrix[r][c]==target:
+#                 return True
+#             elif matrix[r][c]>target:
+#                 h=mid-1
+#             else:
+#                 l=mid+1
+#         return False
+

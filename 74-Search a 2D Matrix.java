44f4b491a2e3fdb0a294b7a7da9cc72e3a2cb6bf
Amey Sunil Kanunje <115860258+ameykanunje@users.noreply.github.com>
2023-06-03 15:57:41 -0400

    Add files via upload

---
+++ 74-Search a 2D Matrix.java
@@ -0,0 +1,28 @@
+public class MatrixSearch {
+    
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int low=0;
+            int high = (matrix.length*matrix[0].length)-1;
+	
+	//execute the code until low is less than or equal to high
+            while(low<=high)
+            {
+                int mid= low+(high-low)/2;
+                int r = mid/matrix[0].length;
+                int c = mid%matrix[0].length;
+    
+                if(matrix[r][c] == target) return true;
+                else if(matrix[r][c] > target)
+                {
+                    high=mid-1;
+                }
+                else if(matrix[r][c] < target)
+                {
+                    low=mid+1;
+                }
+            }
+            return false;
+            
+        }
+    
+}

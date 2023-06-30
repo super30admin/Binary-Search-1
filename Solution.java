0c4368b2909472909204dc99c12acc2a100a9ecc
somonnoy <sb7238@g.rit.edu>
2023-06-29 22:21:13 -0400

    completed Search 2D matrix

--- Solution.java
+++ Solution.java
@@ -1,22 +1,35 @@
+
+// Time Complexity : O(log(m * n))
+// Space Complexity : O(1) because we only use a constant amount of extra space
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
 
-        int m = matrix.length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        int n = matrix[0].length;
+        int left = 0;
+        int right = rows * cols - 1; // Finding index of last element of flattened matrix
 
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(matrix[i][j] == target)
-                {
-                    return true;
-                }
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Finding mid index of flattened matrix
+            int midValue = matrix[mid / cols][mid % cols]; // Finding mid value of flattened matrix
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
         return false;
-
     }
 }

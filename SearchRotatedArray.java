6f0063a039d92fdf413e58d65557494bd63e4d12
Sandesh Shalavadi <sandeshshalavadi1995@gmail.com>
2023-08-09 12:44:57 -0700

    Update SearchRotatedArray.java

--- SearchRotatedArray.java
+++ SearchRotatedArray.java
@@ -7,11 +7,17 @@ class Solution {
         int start = 0, end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
+
+            // find target
             if (nums[mid] == target) return mid;
+
+            // mid's left is sorted (left subarray)
             else if (nums[mid] >= nums[start]) {
                 if (target >= nums[start] && target < nums[mid]) end = mid - 1;
                 else start = mid + 1;
             }
+
+            // // mid's right is sorted (right subarray)
             else {
                 if (target <= nums[end] && target > nums[mid]) start = mid + 1;
                 else end = mid - 1;

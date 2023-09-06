c05f596c4806330173883f6aef86c4eea600bfd9
vinith bandoju <vinith007.bandoju.com>
2023-09-05 20:59:37 -0700

    Search in array of unkown size

--- SearchInSortedArrayOfUnkownSize.java
+++ SearchInSortedArrayOfUnkownSize.java
@@ -1,3 +1,21 @@
 public class SearchInSortedArrayOfUnkownSize {
-    //Yet to be discussed in class
+    public int search(ArrayReader reader, int target) {
+        int low  = 0, high = 1;
+        while(reader.get(high)<target){
+
+            low = high;
+            high = high * 2;
+        }
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(target==reader.get(mid))
+                return mid;
+            else if(reader.get(mid)>target)
+                high = mid -1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+
+    }
 }

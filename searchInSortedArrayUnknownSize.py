339bffc9e9495d403b044c19846292d75268a9dd
anaghayn <anagha2605@gmail.com>
2023-09-08 22:13:02 -0700

    Update searchInSortedArrayUnknownSize.py

--- searchInSortedArrayUnknownSize.py
+++ searchInSortedArrayUnknownSize.py
@@ -1,24 +1,36 @@
-# Time Complexity: O(n)
+# Time Complexity : O(log(n))
 # Space Complexity: O(1)
 
+
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader(object):
+#    def get(self, index):
+#        """
+#        :type index: int
+#        :rtype int
+#        """
+
 class Solution(object):
-    def longestPalindrome(self, s):
+    def search(self, reader, target):
         """
-        :type s: str
+        :type reader: ArrayReader
+        :type target: int
         :rtype: int
         """
-
-        dic=[]
-        count =0
-        if (len(s)==1):
-            return 1
-        for i in s:
-            if i not in dic:
-                dic.append(i)
+        low ,high = 0, 1
+        while reader.get(high) < target:
+            low = high
+            high = 2*high
+        while low <= high:
+            mid = (low+high) // 2
+            if reader.get(mid) == target:
+                return mid
+            elif reader.get(mid) > target:
+                high = mid - 1
             else:
-                dic.remove(i)
-                count+=2
-        if (len(dic) !=0):
-            count += 1
-        
-        return count
+                low = mid + 1
+
+        return -1

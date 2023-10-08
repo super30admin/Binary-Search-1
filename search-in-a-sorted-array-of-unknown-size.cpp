fc51b82ca9c9b670bdc0decbd6e564755d54ba44
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-08 15:35:20 -0700

    Update search-in-a-sorted-array-of-unknown-size.cpp

--- search-in-a-sorted-array-of-unknown-size.cpp
+++ search-in-a-sorted-array-of-unknown-size.cpp
@@ -10,22 +10,19 @@
 class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
-        if(reader.get(0)==target)return 0;
-        if(reader.get(1)==target)return 1;
-
-        int low =0, high = 1;
-        while(low<=high){
-            if(target > reader.get(high)){
-                low = high;
-                high *= 2;
-            }
-            else if(target<=reader.get(high)){
-                int mid = low+(high-low)/2;
-                if(reader.get(mid)==target)return mid;
-                else if(target > reader.get(mid))low=mid+1;
-                else high = mid-1;
-            }
-        }
-        return -1;
+     int low=0, high=1;
+     while(reader.get(high)< target){
+         low = high;
+         high = 2*high;
+     }
+     while(low<=high){
+         int mid = low+(high-low)/2;
+         if(reader.get(mid)== target){
+             return mid;
+         }
+         else if(reader.get(mid)>target)high = mid-1;
+         else low=mid+1;
+     }
+     return -1;
     }
 };

51903ed9aec7e7e8a21c1a5d1254fbf1bcc9beaf
Yatish Koul <36380820+yatishkoul@users.noreply.github.com>
2023-10-08 16:59:45 -0700

    Update search-in-rotated-sorted-array.cpp

--- search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array.cpp
@@ -1,21 +1,37 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
 class Solution {
 public:
     int search(vector<int>& a, int target) {
-    int n = a.size();
-    int low=0;
-    int high=n-1;
-    while(low<high){
-        int mid= low+(high-low)/2;
-        if(a[0]>target ^ a[0]>a[mid] ^ target > a[mid])low=mid+1;
-        else high = mid;
-
-    }
-    return ((low==high && a[low]==target) ? low:-1);
+   if(a.size()==0 )return -1;
+   int low = 0, high = a.size()-1;
+   while(low<=high){
+       int mid = low + (high-low)/2;
+       if(a[mid]== target)return mid;
+       else if(a[low]<= a[mid]){// if left is sorted
+           if(a[low]<=target && a[mid]>target){
+               high = mid-1;
+           }
+           else low = mid+1;
+       }
+       else{// if right is sorted
+        if(a[mid] < target && a[high]>= target){
+            low = mid+1;
+        }
+        else high = mid-1;
+       }
+   }
+   return -1;
 }
 };
-// Your code here along with comments explaining your approach in three sentences only
+
+
+
+ // int n = a.size();
+    // int low=0;
+    // int high=n-1;
+    // while(low<high){
+    //     int mid= low+(high-low)/2;
+    //     if(a[0]>target ^ a[0]>a[mid] ^ target > a[mid])low=mid+1;
+    //     else high = mid;
+
+    // }
+    // return ((low==high && a[low]==target) ? low:-1);

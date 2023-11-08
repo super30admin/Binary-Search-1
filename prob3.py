e72af1187164d2d9c3a34600a9b04fe5d62fc375
Merin Joseph <merinjputhiyedath@gmail.com>
2023-11-08 00:30:37 -0800

    made some changes to prob3

--- prob3.py
+++ prob3.py
@@ -1,3 +1,10 @@
+# // Time Complexity :O(logn)
+# // Space Complexity :O(1)
+# // Did this code successfully run on Leetcode :Yes
+# // Any problem you faced while coding this :No
+
+# we devide the problem space into two halves using the mid pointer and discard one half of the problem by chekcing in which half the target value falls in. and update 
+# the boundaries of start and end according to which half gets selected. 
 left, right = 0, 20000
 while left < right:
     mid = (left + right) >> 1

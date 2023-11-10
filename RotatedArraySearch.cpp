dcc81628555cf10ab9dc1b287ffe8b12f6b8d580
Bhagya <bhagyashreejoshi@csu.fullerton.edu>
2023-11-10 18:33:42 -0500

    Completed Problem 6

---
+++ RotatedArraySearch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+         int lo = 0, hi = nums.size();
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        
+        double num = (nums[mid] < nums[0]) == (target < nums[0])
+                   ? nums[mid]
+                   : target < nums[0] ? -INFINITY : INFINITY;
+                   
+        if (num < target)
+            lo = mid + 1;
+        else if (num > target)
+            hi = mid;
+        else
+            return mid;
+    }
+    return -1;
+    }
+};

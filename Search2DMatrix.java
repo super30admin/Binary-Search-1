9112e72ac81bc777eb742880b945ba02387df50e
Anudeep Uppu <Anudeep.Uppu@nau.edu>
2023-12-18 14:50:20 -0700

    Completed

---
+++ Search2DMatrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int rows = matrix.length, cols = matrix[0].length;
+        int low = 0, high = rows * cols - 1, mid, row_index, col_index;
+
+        while(low <= high)
+        {
+            mid = low + (high - low) / 2;
+
+            row_index = mid / cols;
+            col_index = mid % cols;
+
+            if(matrix[row_index][col_index] == target) return true;
+
+            else if(matrix[row_index][col_index] < target) low = mid + 1;
+
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+}
---
+++ SearchRotatedSortedArray.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[low] <= nums[mid])
+            {
+                //This means left part of the array is in sorted form.
+
+                if(target >= nums[low] && target <= nums[mid])
+                {
+                    //target element is in this range.
+                    high = mid - 1;
+                }
+                else
+                {
+                    //target element is not present in the left sorted array
+                    low = mid + 1;
+                }
+            }
+            else if(target >= nums[mid] && target <= nums[high])
+            {
+                //right part of the array is sorted.
+                low = mid + 1;
+            }
+            else
+            {
+                //right part of the array is sorted but the element is present in the
+                // left array which is unsorted
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }  
+}

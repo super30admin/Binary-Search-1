b12ef48d9e298e86e4419f6e1357a75e4922a907
Gali Bhaskar <luciferbhaskar@gmail.com>
2023-12-22 16:08:28 -0700

    Completed Binary-Search-1

---
+++ RotatedSortedArrayTester.java
@@ -0,0 +1,9 @@
+public class RotatedSortedArrayTester {
+    public static void main(String[] args) {
+        RotatedSortedArray obj = new RotatedSortedArray();
+
+        int[] nums = { 8, 9, 1, 3, 5 };
+
+        System.out.println(obj.search(nums, 50));
+    }
+}

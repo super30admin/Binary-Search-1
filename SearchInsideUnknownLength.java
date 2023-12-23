80497e5c2ebc038acd010b459b458de39ed7a516
Monisha Sydam <112054217+Monisha3696@users.noreply.github.com>
2023-12-22 20:40:17 -0600

    Update SearchInsideUnknownLength.java

--- SearchInsideUnknownLength.java
+++ SearchInsideUnknownLength.java
@@ -1,1 +1,25 @@
+//Time complexity O(log n)
+//Space Complexity O(1)
+//Yes
 
+class Solution {
+    public int search(ArrayReader reader, int target) {
+        int hi = 1;
+        while (reader.get(hi) < target) {
+            hi = hi * 2;
+        }
+        int low = hi / 2;
+        while (low <= hi) {
+            int mid = low + (hi - low) / 2;
+            int val = reader.get(mid);
+            if (val == target) {
+                return mid;
+            } else if ( val > target) {
+                hi = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}

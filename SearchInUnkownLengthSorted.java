1efc7fcd47fa7ae12f9e5985c816f9e3a0a460a2
Pavan Sai Lankalapalli <pavansailankalapalli@Pavans-MacBook-Air.local>
2024-03-04 13:20:13 -0500

    Completed Binary Search -1

---
+++ SearchInUnkownLengthSorted.java
@@ -0,0 +1,28 @@
+class Solution {
+    /*Time Complexity : O(logn);
+     * Space Complexity : O(n);
+     * 
+     * Executed in leetcode;
+     */
+    public int search(ArrayReader reader, int target) {
+        if (reader.get(0) == target) return 0;
+        int low = 0;
+        int high = 1;
+        while (reader.get(high) < target ){
+            low = high;
+            high = 2 * high;
+        }
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (reader.get(mid) == target){
+                return mid;
+            }else if (reader.get(mid) > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return -1;  
+    }
+}

c085f6ecd0d821e52b8735df9cbf5413c220ef47
sjakka9 <137810851+sjakka9@users.noreply.github.com>
2024-03-08 15:45:24 -0500

    Update SearchInSortedArrayUnKnownSize.java

--- SearchInSortedArrayUnKnownSize.java
+++ SearchInSortedArrayUnKnownSize.java
@@ -1,6 +1,6 @@
 // Time Complexity: O(log n)
 // Space Complexity: O(1)
-// Did this code successfully run on Leetcode: No
+// Did this code successfully run on Leetcode: Yes
 // Any problem you faced while coding this: No
 
 // Approach:
